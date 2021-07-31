eae2c592c66e62a70b5b978545e1cf79135139f4
Eyob Alemu <it.eyob.alemu@gmail.com>
2021-07-31 15:34:27 +0300

    refactor widgets to methods

--- starter_project_mobile/lib/screens/eyob_page.dart
+++ starter_project_mobile/lib/screens/eyob_page.dart
@@ -16,117 +16,126 @@ class EyobPage extends StatelessWidget {
     );
   }
 
-  SafeArea _buildLayout(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            BlocBuilder<QouteBloc, QouteState>(
-              builder: (context, state) {
-                return Expanded(
-                  child: Center(
-                    child: Image.asset(state.qoute.illustration),
-                  ),
-                  flex: 2,
-                );
-              },
-            ),
-            SizedBox(
-              height: 5.0,
-            ),
-            Expanded(
-              flex: 1,
-              child: BlocBuilder<QouteBloc, QouteState>(
-                builder: (context, state) {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(state.qoute.author,
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline5
-                              ?.copyWith(
-                                  fontSize: 24.0, fontWeight: FontWeight.w800)),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                        child: Text(
-                          state.qoute.text,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              letterSpacing: .2,
-                              fontWeight: FontWeight.w500,
-                              fontSize: 18.0,
-                              color: Colors.grey[800]),
-                        ),
-                      )
-                    ],
-                  );
-                },
+  Widget _buildLayout(BuildContext context) {
+    return GestureDetector(
+      onHorizontalDragStart: (_) {},
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: BlocBuilder<QouteBloc, QouteState>(
+                  builder: (context, state) {
+                    return _buildQouteDisplay(context, state);
+                  },
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 15.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  FloatingActionButton(
-                    heroTag: "prev btn",
-                    onPressed: () {
-                      context.read<QouteBloc>().add(QouteEvent.prev);
-                    },
-                    child: Icon(
-                      Icons.arrow_left,
-                      size: 35.0,
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    FloatingActionButton(
+                      heroTag: "prev btn",
+                      onPressed: () {
+                        context.read<QouteBloc>().add(QouteEvent.prev);
+                      },
+                      child: Icon(
+                        Icons.arrow_left,
+                        size: 35.0,
+                      ),
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black,
                     ),
-                    backgroundColor: Colors.white,
-                    foregroundColor: Colors.black,
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      CircleAvatar(
-                        radius: 5.0,
-                        backgroundColor: Colors.white,
-                      ),
-                      SizedBox(
-                        width: 5.0,
-                      ),
-                      CircleAvatar(
-                        radius: 5.0,
-                        backgroundColor: Colors.black,
-                      ),
-                      SizedBox(
-                        width: 5.0,
-                      ),
-                      CircleAvatar(
-                        radius: 5.0,
-                        backgroundColor: Colors.white,
-                      )
-                    ],
-                  ),
-                  FloatingActionButton(
-                    heroTag: "next btn",
-                    onPressed: () {
-                      BlocProvider.of<QouteBloc>(context).add(QouteEvent.next);
-                    },
-                    child: Icon(
-                      Icons.arrow_right,
-                      size: 30.0,
+                    BlocBuilder<QouteBloc, QouteState>(
+                      builder: (context, state) {
+                        return _buildProgressDisplay(state);
+                      },
                     ),
-                    backgroundColor: Colors.black,
-                    foregroundColor: Colors.white,
-                  ),
-                ],
-              ),
-            )
-          ],
+                    FloatingActionButton(
+                      heroTag: "next btn",
+                      onPressed: () {
+                        context.read<QouteBloc>().add(QouteEvent.prev);
+                      },
+                      child: Icon(
+                        Icons.arrow_right,
+                        size: 30.0,
+                      ),
+                      backgroundColor: Colors.black,
+                      foregroundColor: Colors.white,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _buildProgressDisplay(QouteState state) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          radius: 5.0,
+          backgroundColor: state.index == 0 ? Colors.black : Colors.white,
+        ),
+        SizedBox(
+          width: 5.0,
+        ),
+        CircleAvatar(
+          radius: 5.0,
+          backgroundColor: state.index == 1 ? Colors.black : Colors.white,
+        ),
+        SizedBox(
+          width: 5.0,
+        ),
+        CircleAvatar(
+          radius: 5.0,
+          backgroundColor: state.index == 2 ? Colors.black : Colors.white,
+        )
+      ],
+    );
+  }
+
+  Widget _buildQouteDisplay(BuildContext context, QouteState state) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Image.asset(state.qoute.illustration),
+        ),
+        SizedBox(
+          height: 50.0,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          child: Text(
+            state.qoute.text,
+            textAlign: TextAlign.center,
+            style: Theme.of(context)
+                .textTheme
+                .headline5
+                ?.copyWith(fontSize: 22.0, fontWeight: FontWeight.w800),
+          ),
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Text(
+          state.qoute.author,
+          style: TextStyle(
+              letterSpacing: .2,
+              fontWeight: FontWeight.w500,
+              fontSize: 18.0,
+              color: Colors.grey[800]),
+        )
+      ],
     );
   }
 }
