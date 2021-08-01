48733d3ec8c8f0ad68d7b55e94e7bb3d61c50ce4
Biruk Ayalew <se.biruk.ayalew@gmail.com>
2021-08-01 20:07:21 +0300

    Create post task

--- starter-project-backend/src/routes/task.ts
+++ starter-project-backend/src/routes/task.ts
@@ -2,8 +2,12 @@ import express from 'express';
 
 import { getTasks } from '../controllers/task';
 
+import { postTask } from '../controllers/task';
+
 const router = express.Router();
 
 router.get('/', getTasks);
+
+router.post('/', postTask);
 
 export default router;
