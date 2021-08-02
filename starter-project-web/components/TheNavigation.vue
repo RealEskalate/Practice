06459c41eb566589b03e14a1415d50291920edb1
Adona Tesfaye <adonatesfaye99@gmail.com>
2021-08-02 19:17:25 +0300

    Add a page

--- starter-project-web/components/TheNavigation.vue
+++ starter-project-web/components/TheNavigation.vue
@@ -1,23 +1,31 @@
 <template>
   <v-card>
     <v-app-bar
-
       app
       class="purple"
       elevate-on-scroll
       scroll-target="#scrolling-techniques-7"
     >
       <v-toolbar-title>{{ title }}</v-toolbar-title>
-
+      <v-spacer />
+      <v-list class="purple">
+        <v-list-tile>
+          <v-list-tile-content>
+            <NuxtLink to="/adona">
+              <v-list-tile-title class="white--text"> Adona </v-list-tile-title>
+            </NuxtLink>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-app-bar>
   </v-card>
 </template>
 <script>
 export default {
-  data () {
+  data() {
     return {
-      title: 'Web Team Members Dashboard'
-    }
-  }
-}
+      title: "Web Team Members Dashboard",
+    };
+  },
+};
 </script>
