de2e28d908a061c0c3942d48bc385d76363b604a
Adona Tesfaye <adonatesfaye99@gmail.com>
2021-08-02 22:16:51 +0300

    fetch posts

--- starter-project-web/pages/adona.vue
+++ starter-project-web/pages/adona.vue
@@ -23,31 +23,7 @@
       </v-col>
 
       <v-col cols="12">
-        <v-card color="white">
-          <v-card-title class="black--text">
-            <h3>Adona's Posts</h3>
-          </v-card-title>
-          <v-card-action>
-            <v-btn left>
-              Add Post
-            </v-btn>
-          </v-card-action>
-          <v-spacer />
-
-          <v-list-item class="black--text">
-            <v-list-item-content>
-              <v-list-item-title>
-                Post 1
-              </v-list-item-title>
-              <v-list-item-title>
-                Post 2
-              </v-list-item-title>
-              <v-list-item-title>
-                Post 3
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-card>
+        <AdonaPosts />
       </v-col>
     </v-row>
   </v-container>
@@ -55,8 +31,13 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import AdonaPosts from '../components/AdonaPosts.vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  components: {
+    AdonaPosts
+  }
+})
 </script>
 
 <style scoped></style>
