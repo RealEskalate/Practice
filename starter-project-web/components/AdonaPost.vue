f0ed712f967d9c9515af137ceb4c85c9c709c3c2
Adona Tesfaye <adonatesfaye99@gmail.com>
2021-08-02 22:16:51 +0300

    fetch posts

---
+++ starter-project-web/components/AdonaPost.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+       <v-pagination
+      v-model="page"
+      :length="6"
+    >
+    <v-container class="black--text">
+      <v-list-item two-line >
+        <v-list-item-content>
+          <v-list-item-title class="black--text">
+            {{ post.title }}
+          </v-list-item-title>
+          <v-list-item-subtitle class="grey--text text--darken-2">
+            {{ post.body }}
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider class="grey lighten-3"/>
+    </v-container>
+    </v-pagination>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'AdonaPost',
+  props: {
+    // eslint-disable-next-line vue/require-default-prop
+    post: Object
+  }
+})
+</script>
---
+++ starter-project-web/components/AdonaPosts.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <v-card color="white">
+      <v-card-title class="black--text">
+        <h3>Adona's Posts</h3>
+      </v-card-title>
+      <v-card-action>
+        <v-btn left> Add Post </v-btn>
+      </v-card-action>
+      <v-spacer />
+      <AdonaPost v-for="post in posts" :key="post.id" :post="post" />
+    </v-card>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import axios from 'axios'
+import AdonaPost from './AdonaPost.vue'
+export default Vue.extend({
+  name: 'AdonaPosts',
+  components: {
+    AdonaPost
+  },
+  data () {
+    return {
+      posts: []
+    }
+  },
+  async created () {
+    const response = await axios.get('http://jsonplaceholder.typicode.com/posts')
+    this.posts = response.data
+    console.log(response.data)
+  }
+})
+</script>
