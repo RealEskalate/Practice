67534cebb2c0167872e31b07aaf4a0f39f9d9b1d
feyselmubarek <feyselmubarek@gmail.com>
2021-08-02 22:53:32 +0300

    [mobile] feysel page drawer item added

--- starter_project_mobile/lib/screens/main_drawer.dart
+++ starter_project_mobile/lib/screens/main_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:start_project_mobile/screens/feysel_page.dart';
 import 'package:start_project_mobile/screens/kaleb_page.dart';
 
 class MainDrawer extends StatelessWidget {
@@ -37,6 +38,18 @@ class MainDrawer extends StatelessWidget {
               ),
               onTap: () {
                 Navigator.of(context).pushNamed(KalebPage.RouteName);
+              }),
+          ListTile(
+              leading: Icon(Icons.person),
+              title: Text(
+                "Feysel",
+                style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontSize: 16.0,
+                ),
+              ),
+              onTap: () {
+                Navigator.of(context).pushNamed(FeyselPage.RouteName);
               }),
         ],
       ),
