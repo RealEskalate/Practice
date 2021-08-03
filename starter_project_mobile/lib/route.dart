eb61f80fe01dc9a86a31eee6fa497ee407dedde6
Eyob Alemu <it.eyob.alemu@gmail.com>
2021-08-03 09:25:39 +0300

    fix merge conflict

--- starter_project_mobile/lib/route.dart
+++ starter_project_mobile/lib/route.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:start_project_mobile/screens/Screens.dart';
 
-import 'models/User.dart';
-
 class PageRouter {
   static Route? generateRoute(RouteSettings settings) {
-    late User? user;
     switch (settings.name) {
       case "/":
         return MaterialPageRoute(builder: (context) {
