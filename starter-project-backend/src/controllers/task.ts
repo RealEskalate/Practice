fe8f50b31846e9fccdbe93fe0ae32715829cddb6
Biruk Ayalew <61549951+birukayalew@users.noreply.github.com>
2021-08-03 16:49:51 +0300

    Create post task (#41)

--- starter-project-backend/src/controllers/task.ts
+++ starter-project-backend/src/controllers/task.ts
@@ -11,3 +11,19 @@ export const getTasks = async (req: Request, res: Response) => {
         res.status(400).end();
     }
 }
+export const postTask = async (req: Request, res: Response) => {
+
+    const task = new models.Task({
+        title: req.body.title,
+        isComplete: req.body.isComplete
+    });
+
+    try{
+        const newTask = await task.save();
+        res.status(201).json({data : newTask});
+    }catch (e){
+        console.error(e);
+        res.status(400).end();
+    }
+
+}
