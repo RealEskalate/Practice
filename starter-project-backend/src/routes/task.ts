9244f068b6661aae2eda957d8c11bc0361c176a5
Eden Mesfin <se.eden.mesfin@gmail.com>
2021-08-04 00:34:47 +0300

    Add GET API for completed tasks

--- starter-project-backend/src/routes/task.ts
+++ starter-project-backend/src/routes/task.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getTasks, putTask }
+import { getTasks, putTask, getCompletedTasks }
     from '../controllers/task';
 
 import { postTask } from '../controllers/task';
@@ -8,6 +8,7 @@ import { postTask } from '../controllers/task';
 const router = express.Router();
 
 router.get('/', getTasks);
+router.get('/Completed-Tasks', getCompletedTasks)
 router.put('/:id', putTask);
 
 router.post('/', postTask);
