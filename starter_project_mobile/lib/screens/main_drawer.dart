30467af6dac6ea4d641e6a7b2f0bc70ac4c5f7e4
MahletH <mahletdereje23@gmail.com>
2021-08-03 23:55:23 +0300

    [Mobile] add Mahlet's page to the app

--- starter_project_mobile/lib/screens/main_drawer.dart
+++ starter_project_mobile/lib/screens/main_drawer.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:starter_project_mobile/screens/Screens.dart';
 import 'package:starter_project_mobile/screens/kaleb_page.dart';
+import 'package:starter_project_mobile/screens/mahlet_page.dart';
 import 'package:starter_project_mobile/screens/minasie_page.dart';
 import 'eyob_screen/eyob_page.dart';
 
@@ -82,6 +83,7 @@ class MainDrawer extends StatelessWidget {
               leading: Icon(Icons.person),
               title: Text(
                 "Hanna",
+              
                 style: TextStyle(
                   color: Colors.blueAccent,
                   fontSize: 16.0,
@@ -89,6 +91,18 @@ class MainDrawer extends StatelessWidget {
               ),
               onTap: () {
                 Navigator.of(context).pushNamed(HannaPage.RouteName);
+              }),
+              ListTile(
+              leading: Icon(Icons.person),
+              title: Text(
+                "Mahlet", 
+                style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontSize: 16.0,
+                ),
+              ),
+              onTap: () {
+                Navigator.of(context).pushNamed(MahletPage.RouteName);
               }),
         ],
       ),
