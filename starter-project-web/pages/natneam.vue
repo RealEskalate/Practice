eb90f36ea60f054ee6215301e29a152b01b6827c
Natneam <se.natneam.mesele@gmail.com>
2021-08-04 20:11:00 +0300

    creating and deleting post functionality added

--- starter-project-web/pages/natneam.vue
+++ starter-project-web/pages/natneam.vue
@@ -4,16 +4,16 @@
       >Please fill all the fields with valid data</v-alert
     >
     <v-card class="rounded-lg pa-3 purple lighten-5 black--text">
-      <v-row>
-        <v-col cols="12" md="3" align-self="center">
+      <v-row no-gutters>
+        <v-col cols="12" md="2" align-self="center">
           <v-img
-            lazy-src="https://picsum.photos/id/11/10/6"
-            max-height="150"
-            max-width="250"
-            src="https://picsum.photos/id/11/500/300"
+            lazy-src="/natneam.jpg"
+            max-height="250"
+            max-width="150"
+            src="/natneam.jpg"
           ></v-img>
         </v-col>
-        <v-col cols="12" md="9">
+        <v-col cols="12" md="6">
           <v-list-item two-line>
             <v-list-item-content>
               <v-list-item-title class="grey--text text--darken-3 text-h4">
@@ -25,12 +25,9 @@
             </v-list-item-content>
           </v-list-item>
           <p class="grey--text text--darken-2 body px-4">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis
-            perspiciatis velit tenetur quas porro corrupti dolor qui. Harum
-            dolorum officiis et dolores, ad nesciunt similique sit sequi
-            suscipit non officia ratione dicta possimus. Cumque obcaecati neque
-            ea, sapiente similique pariatur. Aspernatur consequuntur molestias
-            amet dicta eaque necessitatibus sit eos delectus.
+            I am Natneam Mesele - a third year software engineering student at
+            Addis Ababa Institute of Technology. I am interested in web
+            development, algorithms, cyber security, mathematics and philosophy.
           </p>
         </v-col>
       </v-row>
@@ -63,8 +60,9 @@
         <v-btn outlined color="purple" @click="createPost()">Post</v-btn>
       </v-row>
     </v-card>
-
-    <p class="black--text mt-5 pt-5 text-h5">POSTS</p>
+    <div v-if="posts.length > 0">
+      <p class="black--text mt-5 pt-5 text-h5">POSTS</p>
+    </div>
 
     <v-card
       class="my-3 purple lighten-5 pa-5"
@@ -72,8 +70,13 @@
       :key="post.id"
     >
       <v-row no-gutters>
-        <v-col cols="12" class="black--text text-h6">
+        <v-col cols="11" class="black--text text-h6">
           {{ post.title }}
+        </v-col>
+        <v-col cols="1">
+          <v-icon color="purple" @click="deletePost(post.id)">
+            delete_forever
+          </v-icon>
         </v-col>
         <v-col cols="12" class="black--text">
           {{ post.body }}
@@ -120,7 +123,7 @@ export default class Natneam extends Vue {
       this.posts = [
         {
           userId: 1,
-          id: this.posts.length + 1,
+          id: this.posts.length * 1000 + 1,
           title: this.postTitle,
           body: this.postBody
         }
@@ -128,6 +131,12 @@ export default class Natneam extends Vue {
     } else {
       this.showError = true;
     }
+  }
+
+  deletePost(id: any) {
+    this.posts = this.posts.filter(value =>
+      value.id !== id ? value : undefined
+    );
   }
 }
 </script>
