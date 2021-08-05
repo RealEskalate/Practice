4e6bd8775d869e717972861e7829978b8f723647
Meti A <metiade7@gmail.com>
2021-08-05 18:08:37 +0300

    add vuex store

---
+++ starter-project-web/store/meti.js
@@ -0,0 +1,64 @@
+import axios from 'axios';
+export const state = () =>({
+    allPosts : [],
+    blogger : {
+        name : "Meti Adane",
+        description : "An aspiring third year software engineering student",
+        img : '../assets/meti.jpg',
+        stars : '6'
+    }
+});
+
+export const getters = {
+    allPosts : (state) => {return state.allPosts},
+    bloggerInfo : (state) => {return state.blogger}
+};
+
+export const actions = {
+
+    async fetchTodos({ commit }) {
+		const res = await axios.get(
+			"https://jsonplaceholder.typicode.com/todos"
+		);
+            console.log(res.data)
+		commit("setPosts", res.data);
+	},
+    async addPost ({ commit }, title) {
+        const res = await axios.post("https://jsonplaceholder.typicode.com/todos", {
+            title : title, 
+            completed : false
+        });
+
+        commit('addNewPost', res.data);
+    }, 
+
+    async deletePost ({ commit }, id){
+        await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+
+        commit('removePost', id)
+    },
+
+    searchP ({ commit }, searchTerm){
+        commit('searchPost', searchTerm.toLowerCase())
+    }
+
+};
+
+export const mutations = {
+    setPosts: (state, newPosts) => {
+        (state.allPosts = newPosts)
+    }
+        ,
+    addNewPost: (state, newPost) => state.allPosts.unshift(newPost),
+    removePost: (state, id) => {
+        state.allPosts = state.allPosts.filter(post => post.id !== id)
+    },
+    searchPost: (state, searchTerm) =>{
+        state.allPosts = state.allPosts.filter(post => {
+            let postTitle = post.title.toLowerCase();
+            
+            return postTitle.includes(searchTerm);
+        })
+    }
+};
+
