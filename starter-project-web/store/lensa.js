cea4b142bf70cd84ace960561d4ed58897542b17
Lensa <lensabillion67@gmail.com>
2021-08-05 16:46:20 +0300

    lensa.add crud operation js

---
+++ starter-project-web/store/lensa.js
@@ -0,0 +1,53 @@
+export const state = () => ({
+  posts: []
+})
+
+export const mutations = {
+  setPosts (state, posts) {
+    state.posts = posts
+  },
+  newPost: (state, post) => state.posts.unshift(post),
+  removePost (state, id) {
+    state.posts = state.posts.filter(post => post.id !== id)
+  },
+  updatePost (state, updPost) {
+    const index = state.posts.findIndex(post => post.id === updPost.id)
+    if (index !== -1) {
+      state.posts.splice(index, 1, updPost)
+    }
+  }
+}
+
+export const actions = {
+  async fetchPosts ({ commit }) {
+    const response = await this.$axios.get(
+      'http://jsonplaceholder.typicode.com/posts'
+    )
+    commit('setPosts', response.data)
+  },
+  async addPost ({ commit }, { title, body }) {
+    const response = await this.$axios.post(
+      'https://jsonplaceholder.typicode.com/posts',
+      { title, body }
+    )
+    commit('newPost', response.data)
+  },
+  async deletePost ({ commit }, id) {
+    await this.$axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    commit('removePost', id)
+  },
+  async updatePost ({ commit }, updPost) {
+    const response = await this.$axios.put(
+      `https://jsonplaceholder.typicode.com/todos/${updPost.id}`,
+      updPost
+    )
+
+    commit('updatePost', response.data)
+  }
+}
+
+export const getters = {
+  allPosts: (state) => {
+    return state.posts
+  }
+}
