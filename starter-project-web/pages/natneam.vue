24c2d1700a7cf886c611f1584ee180698eb0c478
Natneam <se.natneam.mesele@gmail.com>
2021-08-05 14:22:43 +0300

    Extarcting Info section as a component

--- starter-project-web/pages/natneam.vue
+++ starter-project-web/pages/natneam.vue
@@ -1,34 +1,6 @@
 <template>
   <div>
-    <v-card class="rounded-lg pa-3 purple lighten-5 black--text">
-      <v-row no-gutters>
-        <v-col cols="12" md="2" align-self="center">
-          <v-img
-            lazy-src="/natneam.jpg"
-            max-height="250"
-            max-width="150"
-            src="/natneam.jpg"
-          ></v-img>
-        </v-col>
-        <v-col cols="12" md="6">
-          <v-list-item two-line>
-            <v-list-item-content>
-              <v-list-item-title class="grey--text text--darken-3 text-h4">
-                Natnaem Mesele
-              </v-list-item-title>
-              <v-list-item-subtitle class="grey--text text--darken-1 subtitle">
-                se.natneam.mesele@gmail.com
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <p class="grey--text text--darken-2 body px-4">
-            I am Natneam Mesele - a third year software engineering student at
-            Addis Ababa Institute of Technology. I am interested in web
-            development, algorithms, cyber security, mathematics and philosophy.
-          </p>
-        </v-col>
-      </v-row>
-    </v-card>
+    <InfoCard />
 
     <v-card color="purple lighten-5 mt-6 pa-3">
       <p class="grey--text text-h6 pa-2">Create Post</p>
@@ -57,6 +29,7 @@
         <v-btn outlined color="purple" @click="createPost()">Post</v-btn>
       </v-row>
     </v-card>
+
     <v-alert dense outlined text class="mt-6" type="error" v-show="showError"
       >Please fill all the fields with valid data</v-alert
     >
@@ -97,9 +70,14 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import InfoCard from "@/components/NatneamInfoCard.vue";
 import axios from "axios";
 
-@Component
+@Component({
+  components: {
+    InfoCard
+  }
+})
 export default class Natneam extends Vue {
   private postTitle = "";
   private postBody = "";
@@ -144,15 +122,7 @@ export default class Natneam extends Vue {
   }
 
   async created() {
-    let data = await axios("https://jsonplaceholder.typicode.com/posts");
-    let rawData = data.data.slice(0, 20);
-
-    rawData.forEach((post: any) => {
-      post.show = false;
-      post.comments = [];
-    });
-
-    this.updatePosts(rawData);
+    this.$store.dispatch("natneamStore/fetchPostsFromApi");
   }
 }
 </script>
