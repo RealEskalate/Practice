7e6c6f4c0ef66de91ec2bfd721df7443267b0004
Lydia <lydiagashawtena@gmail.com>
2021-08-07 22:20:23 +0300

    Finalize practice project

--- starter-project-web/pages/lydia.vue
+++ starter-project-web/pages/lydia.vue
@@ -1,23 +1,6 @@
 <template>
   <v-container>
-    <v-row justify="center" align="center">
-      <v-col cols="3">
-        <v-avatar size="200" elevation="100">
-          <img src="/lydia_dummy_picture.jpg" alt="Lydia">
-        </v-avatar>
-      </v-col>
-
-      <v-col cols="6">
-        <h1 class="text-h2">
-          Lydia Gashawtena
-        </h1>
-        <p class="mt-3 text-body-2 font-weight-light">
-          Part-time member of the web development team currently working on
-          projects called TrackSym and Digital Task Listing.
-        </p>
-      </v-col>
-    </v-row>
-
+    <LydiaInfo />
     <v-card-title class="black--text">
       <v-row>
         <v-col cols="10">
@@ -41,6 +24,7 @@
 
     <v-spacer />
     <v-container v-if="textarea_visible" class="px-0">
+      <p>Add Post</p>
       <v-text-field
         label="New Post"
         outlined
@@ -48,9 +32,36 @@
       />
     </v-container>
 
-    <v-card v-for="post in lydia_posts.slice().reverse()" :key="post.id" class="mb-4">
+    <v-container v-if="edit_text_area_visible" class="px-0">
+      <p>Edit Post</p>
+      <v-text-field
+        outlined
+        :value="to_edit.body"
+        @keydown.enter="updatePost($event.target.value)"
+      />
+    </v-container>
+
+    <v-card v-for="post in posts.slice().reverse()" :key="post.id" class="mb-4">
       <v-card-subtitle class="grey--text pb-0">
-        {{ post.title }}
+        <v-row>
+          <v-col cols="10">
+            {{ post.title }}
+          </v-col>
+          <v-col cols="1">
+            <v-btn left text class="grey--text" @click="handleEditClick(post)">
+              <v-icon>
+                mdi-pencil
+              </v-icon>
+            </v-btn>
+          </v-col>
+          <v-col cols="1">
+            <v-btn left text class="grey--text" @click="deletePost(post.id)">
+              <v-icon>
+                mdi-delete
+              </v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
       </v-card-subtitle>
       <v-card-text class="black--text">
         {{ post.body }}
@@ -59,32 +70,46 @@
   </v-container>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from 'vue'
+import { mapState, mapActions } from 'vuex'
+import LydiaInfo from '@/components/LydiaInfo.vue'
 
 export default Vue.extend({
+  components: {
+    LydiaInfo
+  },
   data () {
     return {
-      textarea_visible: false
+      textarea_visible: false,
+      to_edit: {},
+      edit_text_area_visible: false
     }
   },
   computed: {
-    lydia_posts () {
-      return this.$store.state.lydia.posts
-    },
-    next_id () {
-      return this.$store.state.lydia.posts[this.$store.state.lydia.posts.length - 1].id + 1
-    }
+    ...mapState('lydia', ['posts'])
   },
   created () {
-    this.$store.dispatch('lydia/fetchPosts')
+    this.fetchPosts()
   },
   methods: {
+    ...mapActions('lydia', [
+      'fetchPosts',
+      'addPost',
+      'editPost',
+      'deletePost'
+    ]),
+    updatePost (content) {
+      this.editPost({ id: this.to_edit.id, body: content, userId: this.to_edit.title, title: this.to_edit.title })
+    },
     handleClick () {
       this.textarea_visible = !this.textarea_visible
+      this.edit_text_area_visible = false
     },
-    addPost (newContent: String) {
-      this.$store.dispatch('lydia/addPost', newContent)
+    handleEditClick (post) {
+      this.edit_text_area_visible = !this.edit_text_area_visible
+      this.to_edit = post
+      this.textarea_visible = false
     }
   }
 })
--- starter-project-web/store/lydia.js
+++ starter-project-web/store/lydia.js
@@ -1,30 +1,44 @@
-import axios from 'axios'
-
 export const state = () => ({
   posts: [
   ]
 })
 
 export const mutations = {
+  setPosts (state, posts) {
+    state.posts = posts
+  },
   addPost (state, post) {
     state.posts.push(post)
   },
-  setPosts (state, posts) {
-    state.posts = posts
+  editPost (state, post) {
+    state.posts = state.posts.filter(item => item.id !== post.id)
+    state.posts.push(post)
+  },
+  deletePost (state, id) {
+    state.posts = state.posts.filter(post => post.id !== id)
   }
 }
 
 export const actions = {
-  addPost ({ commit }, content) {
+  async fetchPosts ({ commit }) {
+    const response = await this.$axios.get('http://jsonplaceholder.typicode.com/posts?userId=1')
+    commit('setPosts', response.data)
+  },
+  async addPost ({ commit }, content) {
     const post = {
-      user_id: 1,
+      userId: 1,
       title: 'My new post title',
       body: content
     }
-    commit('addPost', post)
+    const response = await this.$axios.post('http://jsonplaceholder.typicode.com/posts/', post)
+    commit('addPost', response.data)
   },
-  async fetchPosts ({ commit }) {
-    const response = await axios.get('http://jsonplaceholder.typicode.com/posts?userId=1')
-    commit('setPosts', response.data)
+  async editPost ({ commit }, post) {
+    const response = await this.$axios.put(`http://jsonplaceholder.typicode.com/posts/${post.id}`, post)
+    commit('editPost', response.data)
+  },
+  async deletePost ({ commit }, id) {
+    await this.$axios.delete(`http://jsonplaceholder.typicode.com/posts/${id}`)
+    commit('deletePost', id)
   }
 }
