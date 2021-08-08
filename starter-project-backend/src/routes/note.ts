f35c7bcb25b8dafd577e83f7e8db3201430889b3
Physix <61460429+Kaleab-git@users.noreply.github.com>
2021-08-08 12:32:20 +0300

    Revert "move putNote to note controller."
    This reverts commit f909fbc95ab820c6124f3e7037e975aa4c33be84.

--- starter-project-backend/src/routes/note.ts
+++ starter-project-backend/src/routes/note.ts
@@ -1,16 +1,12 @@
 import express from 'express';
 
-import { deleteNoteById, getAllNotes, addNote, putNote } from '../controllers/note';
+import {deleteNoteById,  getAllNotes, addNote } from '../controllers/note';
 
 import { getNoteById } from '../controllers/note';
-
 const router = express.Router();
 
 router.get("/", getAllNotes);
-
 router.post("/", addNote);
-
-router.put('/:id', putNote);
 
 router.delete("/delete/:id", deleteNoteById);
 
