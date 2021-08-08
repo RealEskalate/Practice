492630cfc3f6031f0baca55ae0a7d103712992b1
Adona Tesfaye <adonatesfaye99@gmail.com>
2021-08-09 00:51:47 +0300

    edit info page

--- starter-project-web/components/AdonaInfo.vue
+++ starter-project-web/components/AdonaInfo.vue
@@ -1,17 +1,10 @@
 <template>
-  <v-card color="white">
-    <v-card-title class="black--text">
-      <h2>Adona Tesfaye</h2>
-    </v-card-title>
+  <v-card color="white" elevation="1">
     <v-container>
       <v-row no-gutters>
         <v-col cols="3">
           <v-list-item-title>
-            <v-img
-              class="circle"
-              lazy-src="https://picsum.photos/id/11/10/6"
-              src="https://picsum.photos/id/11/500/300"
-            />
+            <img src="../assets/adona.jpg">
           </v-list-item-title>
         </v-col>
 
@@ -19,19 +12,17 @@
           <v-list-item class="black--text">
             <v-list-item-content>
               <!-- <v-list-item-title> Adona Tesfaye </v-list-item-title> -->
-
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Distinctio sint dicta corporis quasi inventore odio, deserunt
-                vitae magnam, accusantium atque rerum adipisci id iusto
-                praesentium blanditiis eius beatae facilis repudiandae. Lorem
-                ipsum dolor sit amet consectetur adipisicing elit. Culpa odio
-                blanditiis modi soluta quis architecto, amet est libero alias
-                dolorum rem, sapiente porro possimus consequatur quidem, ullam
-                eaque officiis! Quae!. Lorem, ipsum dolor sit amet consectetur
-                adipisicing elit. Itaque magni ea explicabo iusto dolores, fugit
-                aliquam! Praesentium ab accusamus numquam maxime similique rerum
-                excepturi! Aliquam sapiente dolor quis quibusdam. Non.
+              <h2 class="name" color="purple">
+                Adona Tesaye
+              </h2>
+              <p class="description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+                quo rerum eos maiores hic fugit laudantium rem animi iure minus
+                sunt est fugiat adipisci molestiae harum exercitationem
+                asperiores soluta Lorem ipsum dolor sit amet consectetur,
+                adipisicing elit. Consectetur, rem quisquam mollitia veritatis,
+                eum quas, modi est impedit eos labore earum laborum similique!
+                Aperiam similique, unde officia voluptatibus dolor magni.
               </p>
             </v-list-item-content>
           </v-list-item>
@@ -51,5 +42,14 @@ export default Vue.extend({
 .description {
   display: flex;
   align-items: center;
+}
+img {
+  width: 250;
+  height: 250px;
+  object-fit: cover;
+  border-radius: 4px 0 0 4px;
+}
+.name {
+  margin: 1rem 0;
 }
 </style>
