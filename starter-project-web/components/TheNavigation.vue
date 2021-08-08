3ed3bcf0973760b021b73397c75233cedc08add9
Adona Tesfaye <adonatesfaye99@gmail.com>
2021-08-09 00:51:47 +0300

    edit info page

--- starter-project-web/components/TheNavigation.vue
+++ starter-project-web/components/TheNavigation.vue
@@ -7,16 +7,6 @@
       scroll-target="#scrolling-techniques-7"
     >
       <v-toolbar-title>{{ title }}</v-toolbar-title>
-      <v-spacer />
-      <v-list class="purple">
-        <v-list-tile>
-          <v-list-tile-content>
-            <NuxtLink to="/adona">
-              <v-list-tile-title class="white--text"> Adona </v-list-tile-title>
-            </NuxtLink>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
     </v-app-bar>
   </v-card>
 </template>
