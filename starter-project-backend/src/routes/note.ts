3e2c7ccf07809bebdd9f8ee9c187ecc3fa939d1a
Physix <61460429+Kaleab-git@users.noreply.github.com>
2021-08-08 12:38:36 +0300

    move putNote to note controller

--- starter-project-backend/src/routes/note.ts
+++ starter-project-backend/src/routes/note.ts
@@ -1,15 +1,19 @@
 import express from 'express';
 
-import {deleteNoteById,  getAllNotes, addNote } from '../controllers/note';
+import { deleteNoteById, getAllNotes, addNote, putNote } from '../controllers/note';
 
 import { getNoteById } from '../controllers/note';
+
 const router = express.Router();
 
 router.get("/", getAllNotes);
+
 router.post("/", addNote);
 
 router.delete("/delete/:id", deleteNoteById);
 
 router.get("/:id", getNoteById);
+
+router.put('/:id', putNote);
 
 export default router;
