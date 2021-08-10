7b86f1ab2abe1182397d9585c569576f7df06dda
minas528 <minalem.5d@gmail.com>
2021-08-10 11:02:19 +0300

    [Mobile] add album model and data

---
+++ starter_testing_project_mobile/lib/data/album.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import '../models/album.dart';
+
+class AlbumData {
+// Get single albutm
+  Future<Album> fetchAlbum(http.Client client) async {
+    final response = await client
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      return Album.fromJson(jsonDecode(response.body));
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
+  }
+}
