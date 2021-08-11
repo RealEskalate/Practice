baff78c8416ccc79c17ec8d1f31903e6a3f997bb
hannasamuel20 <hanna17st@gmail.com>
2021-08-11 18:00:09 +0300

    [Mobile] Add files to starter testing project

--- starter_testing_project_mobile/test/todo/fetch_todo_test.dart
+++ starter_testing_project_mobile/test/todo/fetch_todo_test.dart
@@ -5,6 +5,7 @@ import 'package:mockito/mockito.dart';
 import 'package:starter_testing_project_mobile/data/todo.dart';
 import 'package:starter_testing_project_mobile/models/todo.dart';
 
+<<<<<<< HEAD
 import "fetch_todo_test.mocks.dart";
 
 @GenerateMocks([http.Client])
@@ -27,6 +28,36 @@ void main() {
     test('throws an exception if the http call completes with an error', () {
       final client = MockClient();
       final todoData = TodoData();
+=======
+import 'fetch_todo_test.mocks.dart';
+
+// Generate a MockClient using the Mockito package.
+// Create new instances of this class in each test.
+@GenerateMocks([http.Client])
+void main() {
+
+  group('fetchTodo', () {
+    test('returns a Todo if the http call completes successfully', () async {
+      final client = MockClient();
+      final todoData = TodoData();
+
+      // Use Mockito to return a successful response when it calls the
+      // provided http.Client.
+      when(client
+              .get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1')))
+          .thenAnswer((_) async =>
+              http.Response('{"userId": 1, "id": 1, "title": "mock", "completed": true}', 200));
+
+      expect(await todoData.fetchTodo(client), isA<Todo>());
+    });
+
+    test('throws an exception if the http call completes with an error', () {
+      final client = MockClient();
+      final todoData = TodoData();
+
+      // Use Mockito to return an unsuccessful response when it calls the
+      // provided http.Client.
+>>>>>>> [Mobile] Add files to starter testing project
       when(client
               .get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1')))
           .thenAnswer((_) async => http.Response('Not Found', 404));
