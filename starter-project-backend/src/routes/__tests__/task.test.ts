b548ed259fbc5a44b631572610606745c74ccb63
Biruk Solomon <41226525+Infinidrix@users.noreply.github.com>
2021-08-13 06:11:57 +0300

    [test] add putTask test (#85)

---
+++ starter-project-backend/src/routes/__tests__/task.test.ts
@@ -0,0 +1,36 @@
+import Task, {ITask} from "../../models/task";
+import * as dbHandler from "./../../tests/setupdb";
+import request from "supertest";
+import app from "../../app";
+
+beforeAll(async () => {
+    await dbHandler.connect();
+});
+
+afterEach(async () => {
+  await dbHandler.clear()
+});
+afterAll(async () => {
+  await dbHandler.disconnect()
+});
+const agent = request.agent(app);
+
+describe("Tasks", () => {
+    it("should put task by id", async () => {
+        expect.assertions(3);
+        const sampleTask = new Task({
+            title: "Sample Title",
+            isComplete: false
+        });
+        const data = await sampleTask.save();
+
+        const new_object = {title: "Improved Title", isComplete: true}
+        const res = await agent
+            .put(`/api/tasks/${data._id}`)
+            .send(new_object);
+        const newTask = res.body?.data;
+        expect(res.statusCode).toEqual(201);
+        expect(newTask?.title).toEqual(new_object.title);
+        expect(newTask?.isComplete).toEqual(new_object.isComplete);
+    });
+});
