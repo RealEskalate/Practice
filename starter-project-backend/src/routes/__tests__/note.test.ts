6b47ce20f69b0f6db6a431fc24cd6618a18b3377
Henok <henokadane8@gmail.com>
2021-08-13 13:20:11 +0300

    test getAllNotes function and fix bug

--- starter-project-backend/src/routes/__tests__/note.test.ts
+++ starter-project-backend/src/routes/__tests__/note.test.ts
@@ -16,6 +16,42 @@ afterAll(async () => {
 
 const agent = request.agent(app);
 
+/**
+ * test getAllNotes function
+ */
+
+ describe("GET /api/posts", () => {
+  it("get all notes", async () => {
+    expect.assertions(3);
+
+    // mock data
+    const note1: INoteDocument = new Note(
+      {
+        title: 'title1',
+        detail: 'detail1',
+      }
+    );
+
+    const note2: INoteDocument = new Note(
+      {
+        title: 'title2',
+        detail: 'detail2',
+      }
+    );
+
+    // save test post to in-memory db
+    await note1.save();
+    await note2.save();
+
+    const res = await agent.get('/api/notes');
+    const notes = res.body;
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.headers['content-type']).toEqual('application/json; charset=utf-8');
+    expect(notes.length).toEqual(2);
+  });
+});
+
 describe("Note", () => {
   it("get note by id", async () => {
     expect.assertions(3);
