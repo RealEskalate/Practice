bb233d31fab3c843d3346131edb4c1797e05ebe6
Natneam <se.natneam.mesele@gmail.com>
2021-08-19 19:55:39 +0300

    testing mutations and getters

--- starter-project-web/jest.config.js
+++ starter-project-web/jest.config.js
@@ -1,24 +1,20 @@
 module.exports = {
+  globalSetup: "<rootDir>/jest.setup.js", // this line is the only change here
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.common.js'
+    "^@/(.*)$": "<rootDir>/$1",
+    "^~/(.*)$": "<rootDir>/$1",
+    "^vue$": "vue/dist/vue.common.js"
   },
-  moduleFileExtensions: [
-    'ts',
-    'js',
-    'vue',
-    'json'
-  ],
+  moduleFileExtensions: ["ts", "js", "vue", "json"],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
-    '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    "^.+\\.ts$": "ts-jest",
+    "^.+\\.js$": "babel-jest",
+    ".*\\.(vue)$": "vue-jest"
   },
   collectCoverage: true,
   collectCoverageFrom: [
-    '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue'
+    "<rootDir>/components/**/*.vue",
+    "<rootDir>/pages/**/*.vue"
   ],
-  testEnvironment: 'jsdom'
-}
+  testEnvironment: "jsdom"
+};
