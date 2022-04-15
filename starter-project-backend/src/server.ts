0988d1788cdcf2f5b111fd4bb05f163f1adf479b
yohansHailu <emailyohans@gmail.com>
2022-04-15 12:58:07 +0300

    [backend] setting up mongodb to locale just for time being

--- starter-project-backend/src/server.ts
+++ starter-project-backend/src/server.ts
@@ -1,9 +1,11 @@
 import app from "./app"
 import mongoose from 'mongoose';
 
-const PORT = process.env.PORT || 8000
-const DB_URI = process.env.MONGO_URI || "";
+// const PORT = process.env.PORT || 8000
+// const DB_URI = process.env.MONGO_URI || "";
 
+const PORT = 8000
+const DB_URI = "mongodb://127.0.0.1:27017/test";
 // @ts-ignore
 mongoose.connect(DB_URI, {
     useNewUrlParser: true,
