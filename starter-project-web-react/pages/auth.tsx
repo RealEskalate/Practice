11d016b89da5a80fdb5e5824fbb9eb44ba88cbbb
Mintesnot Bezabhi <61611671+Mintaa911@users.noreply.github.com>
2022-04-18 11:30:21 +0300

    [WEB] start auth sign in page
    * install material UI
    
    * [Web] Update README.md file
    
    * [Web] created example files
    
    * [Web] Fix bug
    
    * [WEB] create login card
    
    * [WEB] create login button
    
    * [WEB] update auth page
    
    * [WEB] rebase main

--- starter-project-web-react/pages/auth.tsx
+++ starter-project-web-react/pages/auth.tsx
@@ -1,3 +1,31 @@
+import LoginCard from "../components/minteLoginCard";
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import {green } from '@mui/material/colors';
+import Container from '@mui/material/Container';
 
+const theme = createTheme({
+    palette:{
+        primary: {
+            main: "#4caf50",
+            contrastText: "#fff"
+        },
+        
+    },
+    typography:{
+        fontFamily:"Poppins",
+        fontSize: 14,
+    }
+})
 
-export default {} 
+const AuthPage = ()=>{
+    return (
+        <ThemeProvider theme={theme}>
+            <Container maxWidth="sm">
+                <LoginCard/>
+            </Container>
+        </ThemeProvider>
+    )
+}
+
+export default AuthPage 
+
