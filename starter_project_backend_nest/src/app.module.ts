bdf687f7f6c530306c3a7c8bbb278220a5127c7d
yohansHailu <emailyohans@gmail.com>
2022-04-29 21:19:26 +0300

    [backend] bug fix

--- starter_project_backend_nest/src/app.module.ts
+++ starter_project_backend_nest/src/app.module.ts
@@ -3,28 +3,23 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todo/todos.module';
-import { ArticleModule } from './article/article.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import config from './config/config';
 
 @Module({
   imports: [
-    // ConfigModule.forRoot({
-    //   load: [config],
-    // }),
-    // MongooseModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   inject: [ConfigService],
-    //   useFactory: async (config: ConfigService) => ({
-    //     uri: config.get<string>('MONGO_URI'),
-    //   }),
-    // }),
-
+    ConfigModule.forRoot({
+      load: [config],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_URI'),
+      }),
+    }),
     TodosModule,
-    ArticleModule,
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/test'), // local databse .. there is no database specified
   ],
-
   controllers: [AppController],
   providers: [AppService],
 })
