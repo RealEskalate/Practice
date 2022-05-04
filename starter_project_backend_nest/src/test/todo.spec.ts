e2a068ce76814bf9fca28c12bd35e4a8c2b0c787
RiyadHassen <riyadworku@gmail.com>
2022-05-04 16:51:34 +0300

    added mongoose in memory

---
+++ starter_project_backend_nest/src/test/todo.spec.ts
@@ -0,0 +1,47 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { closeInMongodConnection, rootMongooseTestModule } from './db';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TodoSchema } from '../todo/todos.model';
+import { TodoService } from '../todo/todo.service';
+import { TodoController } from './../todo/todo.controller';
+
+describe('TodoTesting', () => {
+  let service: TodoService;
+  let module: TestingModule;
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      imports: [
+     
+  rootMongooseTestModule(),
+        MongooseModule.forFeature([
+          {
+            name: 'Todo',
+            schema: TodoSchema,
+          },
+        ]),
+      ],
+      providers: [TodoService],
+    }).compile();
+    service = module.get<TodoService>(TodoService);
+  });
+  beforeEach(async () => {
+    const todo = await service.createTodo(
+      'todo title 1',
+      'todo description 1',
+      false,
+    );
+  });
+  describe('check todo exist', () => {
+    it('check if user exist', async () => {
+      const todo = await service.getTodos();
+      expect(todo).toBeDefined();
+    });
+  });
+
+  afterAll(async () => {
+    if (module) {
+      await module.close();
+      await closeInMongodConnection();
+    }
+  });
+});
