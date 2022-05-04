e69e6db7d8c67ae37207f82291dbb1f51b71ba69
ludaze <luhanad@ymail.com>
2022-05-05 00:14:26 +0300

    starter project created

--- starter-project-web-react/pages/_app.tsx
+++ starter-project-web-react/pages/_app.tsx
@@ -1,22 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Layout from '../components/Layout'
-import { SessionProvider } from "next-auth/react"
 
-import { useStore } from '../store/configureStore'
+import Layout from '../components/Layout'
+
+
 import { Provider } from 'react-redux'
-function MyApp({ Component, pageProps }: AppProps) {
-  let store = undefined
-  store = useStore(pageProps.initialReduxState)
-  return (<Provider store={store}>
-          <SessionProvider session={pageProps.session}>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </SessionProvider>
-          </Provider>
-  )
-}
+
 
 export default MyApp
 
--- starter-project-web-react/pages/index.tsx
+++ starter-project-web-react/pages/index.tsx
@@ -1,24 +1,8 @@
-import type { NextPage } from 'next'
-import Bloglist from './blogs'
-import { getSession } from 'next-auth/react'
+
 import axios from 'axios'
 
-const Home: NextPage = () => {
-  return <Bloglist />
+const Home: {
+  return < home/>
 }
 export default Home
-export async function getServerSideProps(context: any) {
-  const session = await getSession(context)
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/auth/login',
-        permanent: false,
-      },
-    }
-  } else {
-  }
-  return {
-    props: { session },
-  }
-}
+
