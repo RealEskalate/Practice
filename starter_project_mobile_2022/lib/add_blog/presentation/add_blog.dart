4a63b2eafda5f588507dd670af618f383f696cc6
Semere Habtu <se.semere.habtu@gmail.com>
2022-05-09 10:45:39 +0300

    added static routeName

--- starter_project_mobile_2022/lib/add_blog/presentation/add_blog.dart
+++ starter_project_mobile_2022/lib/add_blog/presentation/add_blog.dart
@@ -2,6 +2,7 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class AddBlog extends StatefulWidget {
+  static const routeName = "addBlog";
   const AddBlog({Key? key}) : super(key: key);
 
   @override
