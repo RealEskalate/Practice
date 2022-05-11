b798175a59adb4600fecd84dce3b8710ba6fe323
Abdi <se.abdulfeta.dedgba@gmail.com>
2022-05-11 08:53:05 +0300

    hydration problem solved

--- starter-project-web-react/pages/_app.tsx
+++ starter-project-web-react/pages/_app.tsx
@@ -1,12 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout'
-
+import { Provider } from 'react-redux'
+import { useStore } from '../store/configureStore'
 function MyApp({ Component, pageProps }: AppProps) {
+  let store = undefined
+  store = useStore(pageProps.initialReduxState)
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   )
 }
 
