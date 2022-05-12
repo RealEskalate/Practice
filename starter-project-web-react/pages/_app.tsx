eebe1ee2bd1a0b05e23ac0d20847b924c7237604
Mintesnot Bezabhi <61611671+Mintaa911@users.noreply.github.com>
2022-05-12 15:05:20 +0300

    Minte.react starter (#142)
    * [Web] created example files
    
    * [Web] Fix bug
    
    * [WEB] update auth page
    
    * [WEB] rebase main
    
    * [WEB] Finish first phase of auth
    
    * [WEB] address review comments
    
    * install material UI
    
    * [Web] Update README.md file
    
    * [Web] created example files
    
    * [Web] Fix bug
    
    * [WEB] create login card
    
    * [WEB] create login button
    
    * [WEB] update auth page
    
    * [WEB] rebase main
    
    * [WEB] Finish first phase of auth
    
    * [WEB] address review comments
    
    * [WEB] start auth state management
    
    * install material UI
    
    * [Web] created example files
    
    * [Web] Fix bug
    
    * [WEB] create login card
    
    * [WEB] create login button
    
    * [WEB] update auth page
    
    * [WEB] rebase main
    
    * [WEB] Finish first phase of auth
    
    * [WEB] address review comments
    
    * [Web] bug fix
    
    * [WEB] implement pseudo api call
    
    * [WEB] bug fix
    
    * [WEB] address review comments
    
    * [WEB] address comment
    
    * [WEB] bug fix

--- starter-project-web-react/pages/_app.tsx
+++ starter-project-web-react/pages/_app.tsx
@@ -1,17 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import { Provider } from 'react-redux'
 import { useStore } from '../store/configureStore'
+import { PersistGate } from 'redux-persist/integration/react'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  let store = undefined
-  store = useStore(pageProps.initialReduxState)
+  let {store, persistor} = useStore(pageProps.initialReduxState)
   return (
-    <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+        </PersistGate>
+
+      </Provider>
   )
 }
 
