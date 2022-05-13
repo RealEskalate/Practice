2c63702c1e23acfee2401a6324eb48abeb483ff3
aben-bel <abenezer.sbelay@gmail.com>
2022-05-13 09:55:14 +0300

    Implement test for user

--- starter_project_backend_nest/src/auth/auth.service.ts
+++ starter_project_backend_nest/src/auth/auth.service.ts
@@ -25,6 +25,7 @@ export class AuthService {
   }
 
   async login(user: any) {
+    console.log('User: ', user);
     const payload = { username: user.username, sub: user._id.toString() };
     return {
       access_token: this.jwtService.sign(payload),
