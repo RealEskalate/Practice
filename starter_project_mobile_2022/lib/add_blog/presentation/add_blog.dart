f7c2ad44d9d40e4ba09dde50c92d977b1cbeeb6b
Semere Habtu <se.semere.habtu@gmail.com>
2022-05-23 06:54:01 +0300

    add blog main page

--- starter_project_mobile_2022/lib/add_blog/presentation/add_blog.dart
+++ starter_project_mobile_2022/lib/add_blog/presentation/add_blog.dart
@@ -1,131 +1,122 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:floatingpanel/floatingpanel.dart';
 import 'package:flutter/material.dart';
+import 'package:starter_project_mobile_2022/add_blog/widgets/body_text_field.dart';
+import 'package:starter_project_mobile_2022/add_blog/widgets/header_text_field.dart';
+import 'package:starter_project_mobile_2022/add_blog/widgets/linear_flow_widget.dart';
+import 'package:starter_project_mobile_2022/add_blog/widgets/multi_selection_form_field.dart';
 
 class AddBlog extends StatefulWidget {
   static const routeName = "addBlog";
-  const AddBlog({Key? key}) : super(key: key);
+  final String label;
+  const AddBlog({Key? key, this.label = "New Article"}) : super(key: key);
 
   @override
   State<AddBlog> createState() => _AddBlogState();
 }
 
 class _AddBlogState extends State<AddBlog> {
-  int index = 2;
+  final _formKey = GlobalKey<FormState>();
+  List? _tags = [];
+
   @override
   Widget build(BuildContext context) {
-    const items = <Widget>[
-      Icon(
-        Icons.home,
-        size: 30,
-        color: Colors.white,
-      ),
-      Icon(
-        Icons.menu_book,
-        size: 30,
-        color: Colors.white,
-      ),
-      Icon(
-        Icons.publish,
-        size: 30,
-        color: Colors.white,
-      ),
-      Icon(
-        Icons.search,
-        size: 30,
-        color: Colors.white,
-      ),
-      Icon(
-        Icons.person,
-        size: 30,
-        color: Colors.white,
-      ),
-    ];
     return Scaffold(
       appBar: AppBar(
+        title: Text(
+          widget.label,
+          style: const TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+        ),
         backgroundColor: Colors.white54,
         elevation: 0,
-        leading: const IconButton(icon: Icon(Icons.clear), onPressed: null),
+        leading: const IconButton(icon: Icon(Icons.menu), onPressed: null),
       ),
-      body: ListView(
-        children: <Widget>[
-          titleTextField(),
-          bodyTextField(),
-          const SizedBox(height: 20),
-          addButton(),
+      body: Stack(
+        children: [
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 12,
+                  child: ListView(
+                    children: <Widget>[
+                      const HeaderTextField(label: "Add title"),
+                      const HeaderTextField(label: "Add subtitle"),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 25, vertical: 10),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 5,
+                              child: MultiSelectionFormField(
+                                decoration: const InputDecoration(
+                                    border: InputBorder.none),
+                                validator: (tags) => (tags?.length ?? 0) < 1
+                                    ? 'Please add a tag'
+                                    : null,
+                                onSaved: (tags) {
+                                  _tags = tags;
+                                },
+                              ),
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: IconButton(
+                                  onPressed: () {
+                                    if (_formKey.currentState!.validate()) {
+                                      setState(
+                                          () => _formKey.currentState!.save());
+                                    }
+                                  },
+                                  color: const Color(0xff3B71FD),
+                                  icon: const Icon(
+                                      Icons.add_circle_outline_outlined)),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      const BodyTextField(label: "Article Content"),
+                      const SizedBox(height: 10),
+                      // SizedBox(
+                      // child: LinearFlowWidget(
+                      // context: context,
+                      // )),
+                      const SizedBox(height: 20),
+                      addButton(),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: LinearFlowWidget(
+                    context: context,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          FloatBoxPanel(
+            backgroundColor: Colors.blue,
+            contentColor: Colors.white,
+            panelIcon: Icons.clear,
+            panelOpenOffset: 20,
+            panelAnimDuration: 400,
+            panelAnimCurve: Curves.easeInOut,
+            dockAnimCurve: Curves.easeInOut,
+            dockAnimDuration: 1000,
+            dockType: DockType.inside,
+            buttons: const [
+              Icons.attachment,
+              Icons.photo_camera,
+              Icons.video_library,
+              Icons.format_align_center,
+              Icons.text_fields
+            ],
+          )
         ],
-      ),
-      bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: Colors.transparent,
-        color: Colors.greenAccent,
-        height: 70,
-        index: index,
-        items: items,
-        buttonBackgroundColor: Colors.amber,
-        onTap: (index) => setState(() => this.index = index),
-      ),
-    );
-  }
-
-  Widget titleTextField() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-      child: TextFormField(
-        decoration: const InputDecoration(
-          border: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Colors.teal,
-            ),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Colors.orange,
-              width: 2,
-            ),
-          ),
-          labelText: "Add Image and Title",
-          prefixIcon: IconButton(
-            icon: Icon(
-              Icons.image,
-              color: Colors.teal,
-            ),
-            onPressed: null,
-          ),
-        ),
-        maxLength: 100,
-        maxLines: null,
-      ),
-    );
-  }
-
-  Widget bodyTextField() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-      child: Expanded(
-        child: TextFormField(
-          decoration: const InputDecoration(
-            border: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: Colors.teal,
-              ),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: Colors.orange,
-                width: 2,
-              ),
-            ),
-            labelText: "Article Content",
-            prefixIcon: IconButton(
-              icon: Icon(
-                Icons.image,
-                color: Colors.teal,
-              ),
-              onPressed: null,
-            ),
-          ),
-          maxLength: 10000,
-          maxLines: 9,
-        ),
       ),
     );
   }
