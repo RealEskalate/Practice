dec77ae86faa951ae0f935341ffa2e62cb730f05
Elshadai Tegegn <54620095+ElshadaiK@users.noreply.github.com>
2022-05-25 15:32:28 +0300

    Fix bug on validation

--- starter-project-web-react/components/profile/ProfileCard.tsx
+++ starter-project-web-react/components/profile/ProfileCard.tsx
@@ -7,33 +7,26 @@ import Switch from '@mui/material/Switch';
 import Grid from '@mui/material/Grid';
 import CardContent from '@mui/material/CardContent';
 import { Alert, AlertTitle, Button, CardHeader } from '@mui/material';
-import TextField from '@mui/material/TextField';
-import { getAuth, register } from '../../store/slices/auth';
-import { ChangeEvent, useEffect, useState } from 'react';
-import { useDispatch, useSelector, useStore } from 'react-redux';
-import { useRouter } from 'next/router';
+import TextField from '../auth/TextField';
+import { useState } from 'react';
 import * as yup from 'yup'
 import { useSession } from "next-auth/react"
 import { Form, Formik } from 'formik';
 
-const session = {
-  user: {
 
-  name: "",
-  email: ""
-  }
-}
-// const { data: session } = useSession() || user;
 
-const INITIAL_STATE = {
-  firstname: session?.user?.name || "",
-  lastname: session?.user?.name,
-  username: session?.user?.email,
-  password: '',
-  confirmPassword: ''
-}
 
 const ProfileCard = () => {
+  const { data: session } = useSession();
+
+  const INITIAL_STATE = {
+    firstname: session?.user?.name || "",
+    lastname: session?.user?.name,
+    username: session?.user?.email,
+    password: '',
+    confirmPassword: ''
+  }
+
   const [INITIAL_STATE_VALUE, setInitialValue] = useState(INITIAL_STATE)
   const [isUpdated, setIsUpdated] = useState(false)
 
@@ -44,15 +37,6 @@ const ProfileCard = () => {
     password: yup.string().min(8).required("Required"),
     confirmPassword: yup.string().min(8).required("Required")
   })
-
-  const router = useRouter();
-  const authentication = useSelector((state: any) => getAuth(state));
-
-  useEffect(() => {
-    if (authentication.user) {
-      router.push('/')
-    }
-  }, [authentication])
 
   return (
     <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
@@ -110,19 +94,19 @@ const ProfileCard = () => {
           <Form>
             <Grid container spacing={2} >
               <Grid >
-                <TextField name="firstname" label="First Name"  variant="standard"/>
+                <TextField name="firstname" label="First Name" />
               </Grid>
               <Grid >
-                <TextField name="lastname" label="Last Name"  variant="standard"/>
+                <TextField name="lastname" label="Last Name" />
               </Grid>
               <Grid >
-                <TextField name="username" label="Username"   variant="standard"/>
+                <TextField name="username" label="Username" />
               </Grid>
               <Grid>
-                <TextField name="password" label="Password" type="password" variant="standard"/>
+                <TextField name="password" label="Password" type="password"  />
               </Grid>
               <Grid>
-                <TextField name="confirmPassword" label="Confirm Password" type="password"  variant="standard"/>
+                <TextField name="confirmPassword" label="Confirm Password" type="password" />
               </Grid>
               <Grid item xs={12} sx={{ mt: 1 }}>
                 <Button variant="contained" color="success">Update</Button>
@@ -131,12 +115,12 @@ const ProfileCard = () => {
           </Form>
         </Formik>
       </Box>
-      {isUpdated?
-            <Alert severity="success">
-                <AlertTitle>Success</AlertTitle>
-                 <strong>User Successfully Registered!</strong>
-            </Alert>: ""
-          }
+      {isUpdated ?
+        <Alert severity="success">
+          <AlertTitle>Success</AlertTitle>
+          <strong>User Successfully Registered!</strong>
+        </Alert> : ""
+      }
     </Grid>
 
 
