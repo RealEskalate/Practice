15a15420a2532c1ce765ae963744c16f07a94157
Denebaye <davidaddis4939@gmail.com>
2022-05-26 13:23:54 +0300

    [Profile Page]

---
+++ starter_project_mobile_2022/lib/profile_page/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const Color white = Colors.white;
+const Color black = Colors.black;
+const Color blue = Color.fromARGB(255, 55, 106, 237);
+const Color red = Colors.red;
+const Color Blue = Color.fromARGB(255, 55, 106, 237);
+const Color darkBlue = Color.fromARGB(150, 13, 37, 60);
+const Color grey = Colors.grey;
+const Color shadow = Color.fromARGB(20, 82, 130, 255);
+const Color edit_color = Color.fromARGB(200, 67, 154, 229);
+const Color backgroundColor = Color.fromARGB(255, 248, 250, 255);
+const Color some_backgrd = Color.fromARGB(255, 68, 76, 91);
