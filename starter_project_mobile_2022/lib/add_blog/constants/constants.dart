4c51b04e3a65591527a15924e36735bfa28ff891
Semere Habtu <se.semere.habtu@gmail.com>
2022-05-27 19:25:21 +0300

    [mobile] reorganized and added constant fields

--- starter_project_mobile_2022/lib/add_blog/constants/constants.dart
+++ starter_project_mobile_2022/lib/add_blog/constants/constants.dart
@@ -1,42 +1,58 @@
 import 'package:flutter/material.dart';
 
-const pageBackground = Colors.white54;
-
-const titleFontSize = 20;
-const titleFontWeight = FontWeight.bold;
-const titleFontColor = Colors.black;
-
-const tagFontSize = 18;
-const tagFontWeight = FontWeight.normal;
+//COLORS
+const labelColor = Colors.teal;
 const tagFontColor = Colors.white;
-
-const buttonBackgroundColor = Colors.blue;
+const hintColor = Color(0xff979EA5);
+const titleFontColor = Colors.black;
+const pageBackground = Colors.white54;
 const buttonContentColor = Colors.white;
-const buttonHeight = 50;
-const buttonWidth = 200;
-const double buttonBorderRadius = 60;
+const buttonBackgroundColor = Colors.blue;
+const addTagsButtonColor = Color(0xff3B71FD);
+const inputDecorationColor = Color.fromRGBO(123, 139, 178, 0.26);
+
+//FONTSIZES
+const double tagFontSize = 18;
+const double hintTextSize = 19;
+const double labelFontSize = 14;
+const double titleFontSize = 20;
+
+//FONTWEIGHTS
+const tagFontWeight = FontWeight.normal;
+const titleFontWeight = FontWeight.bold;
 const buttonTextFontWeight = FontWeight.bold;
 
+//LABELS
+const tagLabel = "Add Tag";
+const buttonLabel = "Publish";
+const titleLabel = "Add Title";
+const subtitleLabel = "Add Subtitle";
+const bodyTextLabel = "Article Content";
+const tagRequestLabel = 'Please add a tag';
+
+//DECORATION SIZES
+const double buttonHeight = 50;
+const double buttonWidth = 200;
+const int bodyTextMaxLines = 15;
+const int bodyTextMaxLength = 1000;
+const double buttonBorderRadius = 60;
+const int headerTextFieldMaxLength = 100;
+const double inputDecorationBorderRadius = 15;
+
+//ICONS
 const menuIcon = Icons.menu;
-const addTagButtonIcon = Icons.add_circle_outline_outlined;
+const clearIcon = Icons.clear;
 const addImageIcon = Icons.photo_camera;
 const addVideoIcon = Icons.video_library;
 const attachFilesIcon = Icons.attachment;
-const modifyAlignmentIcon = Icons.format_align_center;
 const modifyTextIcon = Icons.text_fields;
-const clearIcon = Icons.clear;
+const modifyAlignmentIcon = Icons.format_align_center;
+const addTagButtonIcon = Icons.add_circle_outline_outlined;
 
-const outerWidgetPadding = EdgeInsets.symmetric(horizontal: 25, vertical: 10);
-const innerWidgetPadding = SizedBox(height: 10);
-
-const inputDecorationColor = Color.fromRGBO(123, 139, 178, 0.26);
+//MARGINS & PADDING
+const int tagAddButtonFlex = 1;
+const int tagFormFieldFlex = 5;
+const double innerWidgetPadding = 10;
 const double inputDecorationWidth = 2;
-const contentPadding =
-    EdgeInsets.only(left: 15, bottom: 11, top: 11, right: 15);
-const double inputDecorationBorderRadius = 15;
-
-const double hintTextSize = 19;
-const hintColor = Color(0xff979EA5);
-
-const labelColor = Colors.teal;
-const double labelFontSize = 14;
+const contentPadding = EdgeInsets.symmetric(horizontal: 15, vertical: 11);
+const outerWidgetPadding = EdgeInsets.symmetric(horizontal: 25, vertical: 10);
