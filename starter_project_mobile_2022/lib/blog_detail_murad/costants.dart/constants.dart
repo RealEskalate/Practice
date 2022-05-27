5d0b039fec114c2a63e32c58d099103e7432b1f2
muradab <se.murad.abdella@gmail.com>
2022-05-27 16:34:29 +0300

    [mobile] Updated blog details UI

--- starter_project_mobile_2022/lib/blog_detail_murad/costants.dart/constants.dart
+++ starter_project_mobile_2022/lib/blog_detail_murad/costants.dart/constants.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
 const Color white = Colors.white;
+const Color transparent = Colors.transparent;
 const Color black = Color.fromARGB(255, 0, 0, 0);
-const Color bgColorCommentDisplay = Color.fromRGBO(246, 246, 246, 1);
+const Color backgroundColorCommentDisplay = Color.fromRGBO(246, 246, 246, 1);
 const Color commentDateColor = Color.fromRGBO(154, 148, 148, 1);
 const Color grey = Colors.grey;
 const Color starYellow = Color.fromARGB(255, 231, 210, 22);
+const Color sendButtonColor = Colors.blueAccent;
+const IconData sendIcon = Icons.send;
+const IconData threeDotsIcon = Icons.more_vert;
+const IconData starIcon = Icons.star;
