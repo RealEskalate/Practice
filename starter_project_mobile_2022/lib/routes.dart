0e2b6897740e58c311251c6c6852fa67a8e9cf2e
shtanriverdi <shtanriverdi@yahoo.com>
2022-05-27 11:16:07 +0300

    Merge pull request #175 from RealEskalate/dave.profilepage
    [mobile] Merge profile page by Daw

--- starter_project_mobile_2022/lib/routes.dart
+++ starter_project_mobile_2022/lib/routes.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:starter_project_mobile_2022/add_blog/presentation/add_blog.dart';
 import 'package:starter_project_mobile_2022/main/homepage.dart';
+import 'package:starter_project_mobile_2022/profile_page/Presentation/Profile_Page.dart';
 
 class PageRouter {
   static Route? generateRoute(RouteSettings settings) {
@@ -9,9 +9,9 @@ class PageRouter {
         return MaterialPageRoute(builder: (context) {
           return const HomePage();
         });
-      case AddBlog.routeName:
+      case ProfilePage.routeName:
         return MaterialPageRoute(builder: (context) {
-          return const AddBlog();
+          return const ProfilePage();
         });
     }
     return null;
