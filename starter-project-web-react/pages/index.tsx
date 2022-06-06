33873d222459614d4e73e93e6177da40926ac486
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2022-06-06 15:25:00 +0300

    [WEB] address review comment

--- starter-project-web-react/pages/index.tsx
+++ starter-project-web-react/pages/index.tsx
@@ -1,15 +1,13 @@
 import type { NextPage } from 'next'
-import Bloglist from './blogs';
-import { getSession } from "next-auth/react"
-import axios from 'axios';
+import Bloglist from './blogs'
+import { getSession } from 'next-auth/react'
+import axios from 'axios'
 
 const Home: NextPage = () => {
-  return (   
-    <Bloglist/>
-)
+  return <Bloglist />
 }
 export default Home
-export async function getServerSideProps(context:any) {
+export async function getServerSideProps(context: any) {
   const session = await getSession(context)
   if (!session) {
     return {
@@ -18,21 +16,19 @@ export async function getServerSideProps(context:any) {
         permanent: false,
       },
     }
-  }else{
+  } else {
     try {
       const res = await axios.get(
         `${process.env.API_BASE_URL}/user/${session.id}`,
-        {headers: { "Authorization": "Bearer " + session.access_token}})
+        { headers: { Authorization: 'Bearer ' + session.access_token } }
+      )
 
-      if(res.status === 200){
-        session.user = {name: res.data.fullName, email: res.data.email} 
+      if (res.status === 200) {
+        session.user = { name: res.data.fullName, email: res.data.email }
       }
-
-    } catch (error: any) {
-      console.log(error.message)
-    }
+    } catch (error: any) {}
   }
   return {
-    props: { session }
+    props: { session },
   }
 }
