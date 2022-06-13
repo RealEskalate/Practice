19ce49fdda4cdd69241e2ccdda22be55f3a6565d
Elshadai Tegegn <54620095+ElshadaiK@users.noreply.github.com>
2022-06-13 17:22:12 -0400

    Cloned

--- starter_project_backend_nest/src/auth/constants.ts
+++
@@ -1,8 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-// export const jwtConstants = {
-//   secret: 'secretKey',
-// };
-
-export const IS_PUBLIC_KEY = 'isPublic';
-export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
