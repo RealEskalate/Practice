2e9d73df66dfa84aa1055214de9b093928965d4b
Elshadai Tegegn <54620095+ElshadaiK@users.noreply.github.com>
2022-06-17 17:25:54 -0400

    Authorization services created

---
+++ starter_project_backend_nest/src/auth/local.strategy.ts
@@ -0,0 +1,20 @@
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super();
+  }
+
+  async validate(email: string, password: string): Promise<any> {
+    const user = await this.authService.validateUser(email, password);
+    const userInfo = user._doc;
+    if (!userInfo) {
+      throw new UnauthorizedException();
+    }
+    return userInfo;
+  }
+}
