f0d1a92ca368c8c32479a5c821b53d50672e22b6
Elshadai Tegegn <54620095+ElshadaiK@users.noreply.github.com>
2022-06-24 17:30:55 -0400

    Wrote code for the testing of the To do controller

---
+++ starter_project_backend_nest/src/todo/todo.controller.ts
@@ -0,0 +1,68 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { Public } from 'src/auth/constants';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { TodoService } from './todo.service';
+import { multerConfig } from './../config/config';
+
+@Controller('/api/todo')
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+  @Public()
+  @Get('/all')
+  getAllTodos() {
+    return this.todoService.getTodos();
+  }
+
+  @Post()
+  async addTodo(
+    @Body('title') title: string,
+    @Body('description') description: string,
+    @Body('completed') completed: boolean,
+  ) {
+    return await this.todoService.createTodo(title, description, completed);
+  }
+  @Get('/comp')
+  async getCompleted() {
+    return await this.todoService.getCompleted();
+  }
+  @Get(':id')
+  getoneTodo(@Param('id') id: string) {
+    return this.todoService.getTodo(id);
+  }
+  @Patch(':id')
+  async updateTodo(
+    @Param('id') todoId: string,
+    @Body('title') title: string,
+    @Body('description') desc: string,
+    @Body('completed') completed: boolean,
+  ) {
+    return await this.todoService.updateTodo(todoId, title, desc, completed);
+  }
+  @Delete(':id')
+  async deleteTodo(@Param('id') todoId: string) {
+    return await this.todoService.deleteTodo(todoId);
+  }
+
+  // don't name parameter of file to be uploaded to 'file' user other names
+  @Public()
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('image'))
+  async uploadImage(@UploadedFile() file: Express.Multer.File) {
+    try {
+      const result = await this.todoService.uploadImageToCloudinary(file);
+      console.log(result['url']);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
