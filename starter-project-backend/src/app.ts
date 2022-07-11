a64399aa4948a0d265aa9f8aad86befa93b6a600
RiyadHassen <riyadworku@gmail.com>
2022-07-11 12:10:10 +0300

    updated backend repo

--- starter-project-backend/src/app.ts
+++ starter-project-backend/src/app.ts
@@ -1,6 +1,5 @@
 import express, {Application, Request, Response, NextFunction, json} from 'express';
 import dotenv from 'dotenv';
-import routes from './routes'
 
 dotenv.config();
 
@@ -10,9 +9,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/", json({}))
-app.use("/api/rate", routes.ratingsRouter)
-
-
-app.use("/api/articles", routes.articleRouter)
 
 export default app
