d4fc2f7b464ca0d4ef3e6cf4562db9b2e96d09b9
Miruts Hadush <miruts.hadush@gmail.com>
2022-07-13 13:42:08 +0300

    set up express api server

---
+++ starter-project-backend/src/server.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const ratingController = require("./controllers/rating.controller");
+
+const port = 3000;
+const host = "127.0.0.1";
+const dbUrl = "mongodb://localhost:27017/blog";
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/api/rating", ratingController.getMany);
+app.get("/api/rating/:id", ratingController.getOne);
+app.post("/api/rating", ratingController.createOne);
+app.put("/api/rating/:id", ratingController.updateOne);
+app.delete("/api/rating/:id", ratingController.removeOne);
+
+const start = async () => {
+  await mongoose.connect(dbUrl, { useNewUrlParser: true });
+  try {
+    app.listen(port, host, () => {
+      console.log(`Server running on http://${host}:${port}`);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+module.exports = { start };
