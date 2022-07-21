603e96a760bc8a2079ba9203191130b8db3781f8
mintuka <mintuka2015@gmail.com>
2022-07-21 17:13:16 +0300

    [backend] add security measures for user

---
+++ starter-project-backend/src/middlewares/login.middleware.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user.models')
+
+const authenticate = async(req, res, next) => {
+    const {email, password} = req.body
+    try{
+        const user = await User.findOne({email})
+        if (user){
+            const auth = await bcrypt.compare(password,user.password)
+            if (auth){
+                const token = jwt.sign({_id: user._id},'jwt token')
+                res.cookie('jwt',token)
+            }
+            else{
+                console.log('pass or email is incorrect')
+            }
+        }
+        throw Error('email not registered')
+        
+    }catch(err){
+        res.status(400).json(err)
+    }
+
+}
+
+module.exports = authenticate
