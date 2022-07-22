0da1c87c9a7d477d596f99b278f39edd04d205e4
Miruts Hadush <miruts.hadush@gmail.com>
2022-07-22 16:24:44 +0300

    [starter-project-backend] add rating tests, configure eslint and prettier

--- starter-project-backend/src/server.js
+++ starter-project-backend/src/server.js
@@ -1,36 +1,36 @@
-const mongoose = require("mongoose");
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const articleRouter = require("./routes/article.routes");
-const userRoutes = require("./routes/user.routes");
-const ratingRouter = require("./routes/rating.router");
-const commentRouter = require("./routes/comment.router");
-const { isAuthenticated } = require("./middleware/isAuthenticated");
+const mongoose = require('mongoose')
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const articleRouter = require('./routes/article.routes')
+const userRoutes = require('./routes/user.routes')
+const ratingRouter = require('./routes/rating.router')
+const commentRouter = require('./routes/comment.router')
+const { isAuthenticated } = require('./middleware/isAuthenticated')
 
-const port = 3000;
-const host = "127.0.0.1";
-const dbUrl = "mongodb://localhost:27017/blog";
-const app = express();
+const port = 3000
+const host = '127.0.0.1'
+const dbUrl = 'mongodb://localhost:27017/blog'
+const app = express()
 
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(morgan("dev"));
-app.use("/api/v1/user", userRoutes);
-app.use("/api/v1/rating", ratingRouter);
-app.use("/api/v1/article", articleRouter); //here router is called for article
-app.use("/api/v1/comments", isAuthenticated, commentRouter);
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(morgan('dev'))
+app.use('/api/v1/user', userRoutes)
+app.use('/api/v1/rating', ratingRouter)
+app.use('/api/v1/article', articleRouter) //here router is called for article
+app.use('/api/v1/comments', isAuthenticated, commentRouter)
 
 const start = async () => {
-  await mongoose.connect(dbUrl, { useNewUrlParser: true });
+  await mongoose.connect(dbUrl, { useNewUrlParser: true })
   try {
     app.listen(port, host, () => {
-      console.log(`Server running on http://${host}:${port}`);
-    });
+      console.log(`Server running on http://${host}:${port}`)
+    })
   } catch (e) {
-    console.error(e);
+    console.error(e)
   }
-};
+}
 
-module.exports = { start };
+module.exports = { start }
--- starter-project-backend/src/utils/crud.js
+++ starter-project-backend/src/utils/crud.js
@@ -1,38 +1,38 @@
 const getOne = (model) => async (req, res) => {
   try {
-    const doc = await model.findOne({ _id: req.params.id }).lean().exec();
+    const doc = await model.findOne({ _id: req.params.id }).lean().exec()
 
     if (!doc) {
-      return res.status(404).end();
+      return res.status(404).end()
     }
 
-    res.status(200).json({ data: doc });
+    res.status(200).json({ data: doc })
   } catch (e) {
-    console.error(e);
-    res.status(400).end();
+    console.error(e)
+    res.status(400).end()
   }
-};
+}
 
 const getMany = (model) => async (req, res) => {
   try {
-    const docs = await model.find().lean().exec();
-    
-    res.status(200).json({ data: docs });
+    const docs = await model.find().lean().exec()
+
+    res.status(200).json({ data: docs })
   } catch (e) {
-    console.error(e);
-    res.status(400).end();
+    console.error(e)
+    res.status(400).end()
   }
-};
+}
 
 const createOne = (model) => async (req, res) => {
   try {
-    const doc = await model.create({ ...req.body });
-    res.status(201).json({ data: doc });
+    const doc = await model.create({ ...req.body })
+    res.status(201).json({ data: doc })
   } catch (e) {
-    console.error(e);
-    res.status(400).end();
+    console.error(e)
+    res.status(400).end()
   }
-};
+}
 
 const updateOne = (model) => async (req, res) => {
   try {
@@ -45,35 +45,35 @@ const updateOne = (model) => async (req, res) => {
         { new: true }
       )
       .lean()
-      .exec();
+      .exec()
 
     if (!updatedDoc) {
-      return res.status(404).end();
+      return res.status(404).end()
     }
 
-    res.status(200).json({ data: updatedDoc });
+    res.status(200).json({ data: updatedDoc })
   } catch (e) {
-    console.error(e);
-    res.status(400).end();
+    console.error(e)
+    res.status(400).end()
   }
-};
+}
 
 const removeOne = (model) => async (req, res) => {
   try {
     const removed = await model.findOneAndRemove({
       _id: req.params.id,
-    });
+    })
 
     if (!removed) {
-      return res.status(404).end();
+      return res.status(404).end()
     }
 
-    return res.status(200).json({ data: removed });
+    return res.status(200).json({ data: removed })
   } catch (e) {
-    console.error(e);
-    res.status(400).end();
+    console.error(e)
+    res.status(400).end()
   }
-};
+}
 
 const crudControllers = (model) => ({
   removeOne: removeOne(model),
@@ -81,6 +81,6 @@ const crudControllers = (model) => ({
   getMany: getMany(model),
   getOne: getOne(model),
   createOne: createOne(model),
-});
+})
 
-module.exports = { crudControllers };
+module.exports = { crudControllers }
