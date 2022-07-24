b692e41b0443c0a8f4c8c4f2fb31d54a51307cf9
mintuka <mintuka2015@gmail.com>
2022-07-24 22:37:21 +0300

    [backend] allow user to upload profile picture

--- starter-project-backend/src/utils/cloudinary.js
+++ starter-project-backend/src/utils/cloudinary.js
@@ -1,13 +1,12 @@
 const cloudinary = require("cloudinary").v2;
-const  cloud_name = 'personalimageupload';
-const  api_key = '432777427965455';
-const  api_secret = 'uFcUbzerNMOfXr_cs1m3xzVSSyU';
+const cloud_name = "personalimageupload";
+const api_key = "432777427965455";
+const api_secret = "uFcUbzerNMOfXr_cs1m3xzVSSyU";
 
 cloudinary.config({
   cloud_name: cloud_name,
   api_key: api_key,
-  api_secret: api_secret
-
+  api_secret: api_secret,
 });
 
 module.exports = cloudinary;
--- starter-project-backend/src/utils/profile.js
+++ starter-project-backend/src/utils/profile.js
@@ -3,18 +3,18 @@ const cloudinary = require("./cloudinary");
 const upload = require("./multer");
 const User = require("../models/user.models");
 
-
 router.delete("/:id", async (req, res) => {
   try {
     let user = await User.findById(req.params.id);
-    user.cloudinary_id && await cloudinary.uploader.destroy(user.cloudinary_id);
+    user.cloudinary_id &&
+      (await cloudinary.uploader.destroy(user.cloudinary_id));
     const data = {
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       email: req.body.email,
       password: user.password,
-      profilePic: '',
-      cloudinary_id: ''
+      profilePic: "",
+      cloudinary_id: "",
     };
     user = await User.findByIdAndUpdate(req.params.id, data, { new: true });
     res.json(user);
@@ -26,7 +26,8 @@ router.delete("/:id", async (req, res) => {
 router.put("/:id", upload.single("image"), async (req, res) => {
   try {
     let user = await User.findById(req.params.id);
-    user.cloudinary_id && await cloudinary.uploader.destroy(user.cloudinary_id);
+    user.cloudinary_id &&
+      (await cloudinary.uploader.destroy(user.cloudinary_id));
     let result;
     if (req.file) {
       result = await cloudinary.uploader.upload(req.file.path);
@@ -46,6 +47,5 @@ router.put("/:id", upload.single("image"), async (req, res) => {
     console.log(err);
   }
 });
-
 
 module.exports = router;
