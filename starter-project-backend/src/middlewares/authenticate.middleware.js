18567c8daa4c224539e5c65ed98736fab1423773
Miruts Hadush <miruts.hadush@gmail.com>
2022-07-25 08:57:27 +0300

    [starter-project-backend] ]resolve dependency issues

--- starter-project-backend/src/middlewares/authenticate.middleware.js
+++ starter-project-backend/src/middlewares/authenticate.middleware.js
@@ -1,45 +1,21 @@
-<<<<<<< HEAD:starter-project-backend/src/middlewares/authenticate.middleware.js
 const jwt = require('jsonwebtoken')
 const secret = 'pass@besp'
 
-const isAuthenticated = async(req, res, next) => {
-    const token = req.cookies.jwt;
-    
-    if (token){
-        jwt.verify(token, `${secret}`, (err,decode) => {
-            if (err){
-                res.redirect('/api/v1/user/login')
-            }else{
-                req.userId = decode._id
-                next()
-            }
-        })
-    }else{
+const isAuthenticated = async (req, res, next) => {
+  const token = req.cookies.jwt
+
+  if (token) {
+    jwt.verify(token, `${secret}`, (err, decode) => {
+      if (err) {
         res.redirect('/api/v1/user/login')
-    }   
+      } else {
+        req.userId = decode._id
+        next()
+      }
+    })
+  } else {
+    res.redirect('/api/v1/user/login')
+  }
 }
 
 module.exports = isAuthenticated
-=======
-const jwt = require("jsonwebtoken");
-
-const isAuthenticated = async (req, res, next) => {
-  console.log("token", req.cookies);
-  const token = req.cookies.jwt;
-
-  if (token) {
-    jwt.verify(token, "jwt token", (err, decode) => {
-      if (err) {
-        res.redirect("/login");
-      } else {
-        console.log(decode);
-        next();
-      }
-    });
-  } else {
-    res.redirect("/login");
-  }
-};
-
-module.exports = { isAuthenticated };
->>>>>>> main:starter-project-backend/src/middleware/isAuthenticated.js
--- starter-project-backend/src/middlewares/login.middleware.js
+++ starter-project-backend/src/middlewares/login.middleware.js
@@ -1,56 +1,26 @@
-<<<<<<< HEAD:starter-project-backend/src/middlewares/login.middleware.js
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const User = require('../models/user.models')
 const secret = 'pass@besp'
 
-
-const authenticate = async(req, res, next) => {
-    const {email, password} = req.body
-    try{
-        const user = await User.findOne({email})
-        if (user){
-            const auth = await bcrypt.compare(password,user.password)
-            if (auth){
-                const token = jwt.sign({_id: user._id}, `${secret}`)
-                console.log("logged in",token)
-                res.cookie('jwt',token)
-            }
-            else{
-                console.log('pass or email is incorrect')
-            }
-        }
-        throw Error('email not registered')
-        
-    }catch(err){
-        res.status(400).json(err)
-=======
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const User = require("../models/user.models");
-
 const authenticate = async (req, res, next) => {
-  const { email, password } = req.body;
-  console.log("email", email);
+  const { email, password } = req.body
   try {
-    const user = await User.findOne({ email });
-    console.log("getUser", user);
+    const user = await User.findOne({ email })
     if (user) {
-      const auth = await bcrypt.compare(password, user.password);
+      const auth = await bcrypt.compare(password, user.password)
       if (auth) {
-        console.log("logged in", user);
-        const token = await jwt.sign({ _id: newUser._id }, "jwt token");
-        res.cookie("jwt", token);
-        res.status(200).json({ user: user._id });
+        const token = jwt.sign({ _id: user._id }, `${secret}`)
+        console.log('logged in', token)
+        res.cookie('jwt', token)
       } else {
-        console.log("pass or email is incorrect");
+        console.log('pass or email is incorrect')
       }
->>>>>>> main:starter-project-backend/src/middleware/logInUser.js
     }
-    throw Error("email not registered");
+    throw Error('email not registered')
   } catch (err) {
-    res.status(400).json(err);
+    res.status(400).json(err)
   }
-};
+}
 
-module.exports = authenticate;
+module.exports = authenticate
