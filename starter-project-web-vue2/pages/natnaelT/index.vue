ed09872ffa9efc02fabb43a64201d3afc47e3e25
Nati_Tedros <tedros.natnael@gmail.com>
2022-07-29 19:40:51 +0300

    Create an Add Blog page

--- starter-project-web-vue2/pages/natnaelT/index.vue
+++ starter-project-web-vue2/pages/natnaelT/index.vue
@@ -1,27 +1,42 @@
 <template>
-  <div>
+  <div class="addblog">
     <h1>Test Blog Page</h1>
     <p>This is where the content of the blog would go! List of all the blogs.</p>
+    <nuxt-link to='/natnaelT/NatnaelAddBlog'>Add a new Blog</nuxt-link>
   </div>
 </template>
 
 <script>
+import NatnaelAddBlog from '~/components/natnaelT/NatnaelAddBlog'
 export default {
-    head(){// used to increase the page's detail for the SEO
+    head() {
         return {
             title: "Blog App",
             meta: [{
-                hid:"description",
-                name:"Blog page",
-                content:"Blog to save thoughts"
-            }]
-        }
+                    hid: "description",
+                    name: "Blog page",
+                    content: "Blog to save thoughts"
+                }]
+        };
+    },
+    comments: {
+        NatnaelAddBlog
     }
 }
 </script>
 
 <style>
-p{
+
+.addblog p{
     font-style: italic;
 }
+
+.addblog nuxt-link{
+    display: inline-block;
+    background: blue;
+    color: aliceblue;
+    padding: 0.3rem 1rem;
+    margin-right: 0.5rem;
+}
+
 </style>
