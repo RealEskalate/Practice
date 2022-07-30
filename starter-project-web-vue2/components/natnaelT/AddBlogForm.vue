0f3021f5a57fb4737d8476019f4d7b035f568c7a
Nati_Tedros <tedros.natnael@gmail.com>
2022-07-30 22:24:22 +0300

    include Create Add and Delete blog functionalities

---
+++ starter-project-web-vue2/components/natnaelT/AddBlogForm.vue
@@ -0,0 +1,86 @@
+<template>
+  <form @submit="onSubmit" class="add-form">
+    <div class="form-control">
+      <label>Title of Blog</label>
+      <input type="text" v-model="title" name="content" placeholder="Blog Title here..." />
+    </div>
+    <div class="form-control">
+      <label>Content of Blog</label>
+      <input
+        type="text"
+        v-model="content"
+        name="content"
+        placeholder="Write here..."
+      />
+    </div>
+    <input type="submit" value="Save Task" class="btn" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'AddBlogForm',
+  data() {
+    return {
+      title: '',
+      content: ''
+    }
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault()
+      if (!this.title) {
+        alert('Please add a task')
+        return
+      }
+      const newBlog = {
+        id: Math.floor(Math.random() * 100000),
+        title: this.title,
+        content: this.content
+      }
+      this.$emit('add-blog', newBlog)
+      this.title = ''
+      this.content = ''
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+.add-form {
+  margin-bottom: 40px;
+}
+
+.form-control {
+  margin: 20px 0;
+}
+
+.form-control label {
+  display: block;
+}
+
+.form-control input {
+  width: 100%;
+  height: 40px;
+  margin: 5px;
+  padding: 3px 7px;
+  font-size: 17px;
+}
+
+.form-control-check {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.form-control-check label {
+  flex: 1;
+}
+
+.form-control-check input {
+  flex: 2;
+  height: 20px;
+}
+
+</style>
---
+++ starter-project-web-vue2/components/natnaelT/SingleBlog.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h3>{{blog.title}}</h3>
+    <p>{{blog.content}}</p>
+    <button class="btn">Update</button>
+    <button @click="$emit('delete-blog',blog.id)" class="btn">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "SingleBlog",
+    props: {
+        blog: Object
+    },
+    emits: ["delete-blog"]
+}
+</script>
+<style>
+
+.btn {
+    display: flex;
+    padding: 2px; 
+    margin: 1px;
+    border-radius: 0.5px;
+}
+
+</style>
