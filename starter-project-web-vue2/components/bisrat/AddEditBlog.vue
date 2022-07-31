83cba40d2c412f814b75897f9ac072b921d42004
bisrat-walle <bisratwalle3@gmail.com>
2022-07-31 19:18:46 +0300

    adding form fields for adding and updating

--- starter-project-web-vue2/components/bisrat/AddEditBlog.vue
+++ starter-project-web-vue2/components/bisrat/AddEditBlog.vue
@@ -1,13 +1,61 @@
 <template>
-  <h2>Add Edit Component {{ hello }}</h2>
+	<div style="margin:auto;max-width:600px !important;padding-bottom:30px">
+  <h2><span v-if="blog.id==null">Add Blog</span><span v-if="blog.id!=null">Update Blog</span></h2>
+  
+  <form>
+    <v-text-field
+      v-model="blog.title"
+	  label="Title"
+    ></v-text-field>
+    <v-textarea
+      v-model="blog.content"
+	  label="Content"
+	  solo
+	  
+    ></v-textarea>
+	
+    <v-btn
+      class="mr-4"
+      @click="onSubmit"
+    >
+      submit
+    </v-btn>
+  </form>
+  </div>
 </template>
 
 <script>
+
+import { mapActions } from "vuex"
+
 export default {
+	name: "AddEditComponent",
+	props: ["updateBlog"],
     data(){
         return {
-            hello: "Hello World"
+			 blog: {
+                id: null,
+                title: "",
+                content: ""
+            },
+			titleRules: [v => v.length > 9 || "title is required and must be at least 10 chars"],
+			contentRules: [v => v.length > 99 || "content is required and must be at least 100 chars"]
         }
     },
+    created(){
+        if (this.updateBlog !== undefined){
+            this.blog = this.updateBlog
+        }
+    },
+    methods: {
+        ...mapActions("bisrat", ["createBlog", "updateBlog"]),
+		onSubmit(){
+			if(this.blog.id == null){
+			  this.createBlog(this.blog);
+			} else {
+			  this.updateBlog(this.blog);
+			} 
+		}
+    }
 }
 </script>
