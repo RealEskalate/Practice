ed51f29f8f76eb7fe1282bb34ec065d9e164ae8c
bisrat-walle <bisratwalle3@gmail.com>
2022-07-31 12:07:21 +0300

    creating AddEditBlog component

---
+++ starter-project-web-vue2/components/bisrat/AddEditBlog.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>Add Edit Component {{ hello }}</h2>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            hello: "Hello World"
+        }
+    },
+}
+</script>
