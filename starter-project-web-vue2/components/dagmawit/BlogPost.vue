f351cfee92e9e934fd904f559e3841ab0b2e4a57
Dagmawit Getachew <se.dagmawit.getachew@gmail.com>
2022-08-02 11:04:39 +0300

    [UI] Blog list UI restructuring

--- starter-project-web-vue2/components/dagmawit/BlogPost.vue
+++ starter-project-web-vue2/components/dagmawit/BlogPost.vue
@@ -1,49 +1,75 @@
 
 <template>
- 
-<v-card>
-    <v-container>
-      <v-row dense v-bind:key="blogPost.id" v-for="blogPost in allBlogPosts">
+  <v-card max-width="900"
+    class="mx-auto">
+    <v-container  >
+      <v-row dense v-bind:key="blogPost.id" v-for="blogPost in allBlogPosts"  >
         <v-col cols="12">
-          <v-card
-            color="#385F73"
-            dark
-          >
-            <v-card-title class="text-h5">
-             {{blogPost.title}}
+          <v-card color="#1F7087" dark>
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                  <v-card-title class="text-h5">
+              {{ blogPost.title }}
             </v-card-title>
 
-            <v-card-subtitle>{{blogPost.content}}</v-card-subtitle>
+             <v-card-subtitle>{{ blogPost.content }}</v-card-subtitle>
+            <v-card-actions>
+              <v-btn text class="grey--text">
+                Learn More
+              </v-btn>
+            </v-card-actions>
+              </div>
+          <div>
+              <v-btn  text class="grey--text">
+            <v-icon> mdi-pencil </v-icon>
+          </v-btn>
+        
+          <v-btn  text class="grey--text">
+            <v-icon> mdi-delete </v-icon>
+          </v-btn>
+          </div>
+           
+
+
+            </div>
+            
+
+
+
+           
+
+
+           
+          
+        
 
             <!-- <v-card-actions>
-              <v-btn text :to="'/dagmawit/' + blog._id">
-                Read More
-              </v-btn>
+              <v-btn text :to="'/dagmawit/' + blog._id"> Read More </v-btn>
             </v-card-actions> -->
           </v-card>
         </v-col>
-
+        
       </v-row>
     </v-container>
   </v-card>
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
-  export default {
-    name: 'BlogPost',
-    methods:{
-      ...mapActions('dagmawit',['fetchBlogPosts'])
-      
-
-    },
-    computed: mapGetters('dagmawit',["allBlogPosts"]),
-    created(){
-      this.fetchBlogPosts()
-    }
-  
-  };
+import { mapGetters, mapActions } from 'vuex'
+export default {
+  name: 'BlogPost',
+  methods: {
+    ...mapActions('dagmawit', ['fetchBlogPosts']),
+  },
+  computed: mapGetters('dagmawit', ['allBlogPosts']),
+  created() {
+    this.fetchBlogPosts()
+  },
+}
 </script>
+
+
+
 
 
 
