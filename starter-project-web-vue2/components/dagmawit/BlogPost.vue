92098b2fd676ee0946e65fb1cb4ca1e225c8133d
Dagmawit Getachew <se.dagmawit.getachew@gmail.com>
2022-08-02 11:43:56 +0300

    [Web] Added a single Page UI and functionality

--- starter-project-web-vue2/components/dagmawit/BlogPost.vue
+++ starter-project-web-vue2/components/dagmawit/BlogPost.vue
@@ -1,54 +1,44 @@
 
 <template>
-  <v-card max-width="900"
-    class="mx-auto">
-    <v-container  >
-      <v-row dense v-bind:key="blogPost.id" v-for="blogPost in allBlogPosts"  >
+  <v-card max-width="900" class="mx-auto">
+    <v-container>
+      <v-row dense v-bind:key="blogPost.id" v-for="blogPost in allBlogPosts">
         <v-col cols="12">
           <v-card color="#1F7087" dark>
             <div class="d-flex flex-no-wrap justify-space-between">
               <div>
-                  <v-card-title class="text-h5">
-              {{ blogPost.title }}
-            </v-card-title>
+                <v-card-title class="text-h5">
+                  {{ blogPost.title }}
+                </v-card-title>
 
-             <v-card-subtitle>{{ blogPost.content }}</v-card-subtitle>
-            <v-card-actions>
-              <v-btn text class="grey--text">
-                Learn More
-              </v-btn>
-            </v-card-actions>
+                <v-card-subtitle>{{ blogPost.content }}</v-card-subtitle>
+                <nuxt-link  :to="'/dagmawit/'  + blogPost._id">
+                  <v-card-actions>
+                    <v-btn
+                      text
+                      class="grey--text"
+                    >
+                      Learn More
+                    </v-btn>
+                  </v-card-actions>
+                </nuxt-link>
               </div>
-          <div>
-              <v-btn  text class="grey--text">
-            <v-icon> mdi-pencil </v-icon>
-          </v-btn>
-        
-          <v-btn  text class="grey--text">
-            <v-icon> mdi-delete </v-icon>
-          </v-btn>
-          </div>
-           
+              <div>
+                <v-btn text class="grey--text">
+                  <v-icon> mdi-pencil </v-icon>
+                </v-btn>
 
-
+                <v-btn text class="grey--text">
+                  <v-icon> mdi-delete </v-icon>
+                </v-btn>
+              </div>
             </div>
-            
-
-
-
-           
-
-
-           
-          
-        
 
             <!-- <v-card-actions>
               <v-btn text :to="'/dagmawit/' + blog._id"> Read More </v-btn>
             </v-card-actions> -->
           </v-card>
         </v-col>
-        
       </v-row>
     </v-container>
   </v-card>
---
+++ starter-project-web-vue2/pages/dagmawit/_id.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <nuxt-link :to="'/dagmawit/'">
+      <v-btn class="ma-2" color="#1F7087" dark>
+        <v-icon dark left> mdi-arrow-left </v-icon>Back
+      </v-btn>
+    </nuxt-link>
+    <h1>Title: {{ blogTitle }}</h1>
+    <hr />
+    <v-container
+      ><h4>{{ blogContent }}</h4></v-container
+    >
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default ({
+    data(){
+        return{
+            blogTitle:'',
+            blogContent:''
+        }
+    },
+
+    async created() {
+  // GET request using axios with async/await
+  const response = await axios.get('https://blog-app-backend.onrender.com/api/articles/'+ this.$route.params.id);
+  this.blogTitle = response.data.blogTitle;
+  this.blogContent = response.data.blogContent;
+
+}
+
+
+    
+})
+</script>
+
+
