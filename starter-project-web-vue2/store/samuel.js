51a4eaafb025c2861b8ad762bb1df5114df753c4
Samuel Abatneh <91486438+samiabat@users.noreply.github.com>
2022-08-02 14:22:22 +0300

    Update samuel.js

--- starter-project-web-vue2/store/samuel.js
+++ starter-project-web-vue2/store/samuel.js
@@ -1,19 +1,17 @@
-import axios from 'axios'
 
 export const state = {
     blogs: [],
 }
 export const actions = {
     async fetchBlogs({ commit }) {
-        const response = await axios.get(
+        const response = await this.$axios.get(
             'https://blog-app-backend.onrender.com/api/articles/all'
         )
         commit('setBlogs', response.data)
     },
     async addBlog({ commit }, blogPost) {
-        console.log(blogPost, "store");
-        const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbXVlbEBnbWFpbC5jb20iLCJzdWIiOiI2MmU2N2Q4ZGUwMDQ4MDdlNWUzMzQ1M2EiLCJpYXQiOjE2NTkyODUwNDIsImV4cCI6MTY1OTI4ODY0Mn0._KVpK16isY9xLk6bQRyVOMOLLEHwFy4jE2a7Ocsz-cU"
-        const response = await axios.post(
+        const token = ''
+        const response = await this.$axios.post(
             'https://blog-app-backend.onrender.com/api/articles',
             blogPost, {
                 headers: {
@@ -25,9 +23,8 @@ export const actions = {
         commit('newBlog', response.data)
     },
     async deleteBlog({ commit }, id) {
-        console.log(id)
-        const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbXVlbEBnbWFpbC5jb20iLCJzdWIiOiI2MmU2N2Q4ZGUwMDQ4MDdlNWUzMzQ1M2EiLCJpYXQiOjE2NTkyODUwNDIsImV4cCI6MTY1OTI4ODY0Mn0._KVpK16isY9xLk6bQRyVOMOLLEHwFy4jE2a7Ocsz-cU"
-        await axios.delete(
+        const token = ''
+        await this.$axios.delete(
             `https://blog-app-backend.onrender.com/api/articles/${id}`, {
                 headers: {
                     Authorization: 'Bearer ' + token,
@@ -39,8 +36,7 @@ export const actions = {
         commit('removeBlog', id)
     },
     async updateBlog({ commit }, updBlog) {
-        const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbXVlbEBnbWFpbC5jb20iLCJzdWIiOiI2MmU2N2Q4ZGUwMDQ4MDdlNWUzMzQ1M2EiLCJpYXQiOjE2NTkyODg5NDYsImV4cCI6MTY1OTI5MjU0Nn0.kATyc9S48O_l02dSVIgb8qE3LtZMqCR6bGpq_GDj-wg"
-        console.log(updBlog, "updblog")
+        const token = ''
         const response = await this.$axios.patch(
             `https://blog-app-backend.onrender.com/api/articles/${updBlog._id}`,
             updBlog, {
