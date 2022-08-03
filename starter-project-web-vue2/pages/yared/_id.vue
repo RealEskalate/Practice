5ced8f1c330d345f82ca0d41b4ba25dd3a62cde9
Yared <90713646+Yared04@users.noreply.github.com>
2022-08-03 16:51:09 +0300

    Yared.demo project UI (#270)
    * create the UI
    
    * [web] implement vuex state management
    
    * [web] implement CRUD functionalities
    
    * [web] add authentication and fix previous comments

---
+++ starter-project-web-vue2/pages/yared/_id.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>{{ blog.title }}</h1>
+    <hr />
+    <div class="text-justify pa-4">
+      <p>{{ blog.content }}</p>
+    </div>
+    <small>Created at: {{ blog.createdAt }}</small>
+    <br />
+    <v-btn absolute style="right: 9%" to="/yared" elevation="2" nuxt
+      >Go Back</v-btn
+    >
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'BlogDetail',
+  async fetch({ store, params }) {
+    try {
+      const response = await store.dispatch('yared/fetchBlog', params.id)
+      this.blog = response.data
+    } catch (e) {
+      // error()
+    }
+  },
+  computed: {
+    ...mapState('yared', ['blog']),
+  },
+}
+</script>
