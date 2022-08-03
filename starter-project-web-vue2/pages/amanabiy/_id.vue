ecbf40b26e00cf1746112b8314473bb4bc58ba57
Amanuel Sisay <amanuelabiy.as@gmail.com>
2022-08-04 01:22:53 +0300

    implemented state management with dummy data

--- starter-project-web-vue2/pages/amanabiy/_id.vue
+++ starter-project-web-vue2/pages/amanabiy/_id.vue
@@ -9,7 +9,7 @@
     <v-card class="mx-6 my-6">
       <v-card-title>
         <span class="text-h6 font-weight-bold"
-          >{{ curr_blog.title }} <small>{{ curr_blog.id }}</small></span
+          >{{ curr_blog.title }}</span
         >
       </v-card-title>
 
@@ -26,6 +26,7 @@
 
 
 <script>
+  import { mapState, mapActions } from 'vuex'
   export default {
     name: 'BlogCurr',
 
@@ -37,40 +38,15 @@
             desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum necessitatibus officia labore illo accusamus temporibus commodi eveniet praesentium ullam exercitationem vel nulla, deleniti modi obcaecati laudantium natus soluta laborum quisquam.",
             author: "Amanuel Sisay",
         },
-        blogs: [
-          {
-            id: "1",
-            title: "Why you should Wake up",
-            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum necessitatibus officia labore illo accusamus temporibus commodi eveniet praesentium ullam exercitationem vel nulla, deleniti modi obcaecati laudantium natus soluta laborum quisquam.",
-            author: "Amanuel Sisay",
-          },
-          {
-            id: "2",
-            title: "Why you should Gym",
-            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum necessitatibus officia labore illo accusamus temporibus commodi eveniet praesentium ullam exercitationem vel nulla, deleniti modi obcaecati laudantium natus soluta laborum quisquam.",
-
-            author: "Abel",
-          },
-          {
-            id: "3",
-            title: "The benifit of Working hard",
-            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum necessitatibus officia labore illo accusamus temporibus commodi eveniet praesentium ullam exercitationem vel nulla, deleniti modi obcaecati laudantium natus soluta laborum quisquam.",
-            author: "Abebe",
-
-          },
-          {
-            id: "4",
-            title: "Interviews Trick",
-            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum necessitatibus officia labore illo accusamus temporibus commodi eveniet praesentium ullam exercitationem vel nulla, deleniti modi obcaecati laudantium natus soluta laborum quisquam.",
-            author: "Kebede",
-
-          },
-        ]
       }
     },
+    computed: { ...mapState('amanabiy', ['blogs'])},
     created () {
+        this.getBlogs()
         this.curr_blog = this.blogs.filter((blog) => blog.id === this.$route.params.id)[0]
     },
+    methods: { ...mapActions('amanabiy', ['getBlogs'])},
+    
   }
 </script>
 
