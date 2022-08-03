f49eb70c4ca9f0285f6e1bfb24dab3992c2d4412
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2022-08-03 16:49:09 +0300

    [web]: folder rename to PascalCase

--- starter-project-web-react/pages/mintesnot/index.tsx
+++ starter-project-web-react/pages/mintesnot/index.tsx
@@ -1,9 +1,10 @@
 import react from 'react'
 import { Box, Container, Grid, Typography, Divider } from '@mui/material'
-import BlogCard from '../../components/mintesnot/blog/blogCard'
-import PostBlog from '../../components/mintesnot/blog/postBlogButton'
+
 import CircularProgress from '@mui/material/CircularProgress'
 import { useFetchBlogsQuery } from '../../store/mintesnot/features/blog/BlogApiSlice'
+import PostBlog from '../../components/mintesnot/blog/postBlogButton'
+import BlogCard from '../../components/mintesnot/blog/blogCard'
 
 const Blogs = () => {
   const { data = [], isFetching } = useFetchBlogsQuery()
