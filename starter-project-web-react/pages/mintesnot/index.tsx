26fe4f95186bd68bd2fe688cbe93a53c93c3d8aa
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2022-08-03 15:35:03 +0300

    [web]: sample structure for the demo

--- starter-project-web-react/pages/mintesnot/index.tsx
+++ starter-project-web-react/pages/mintesnot/index.tsx
@@ -1,6 +1,5 @@
 import react from 'react'
 import { Box, Container, Grid, Typography, Divider } from '@mui/material'
-
 import CircularProgress from '@mui/material/CircularProgress'
 import { useFetchBlogsQuery } from '../../store/mintesnot/blog/BlogApiSlice'
 import PostBlog from '../../components/mintesnot/blog/PostBlogButton'
