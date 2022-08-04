041d38827696ff940878ed5c8025f8c2dc7c822c
Samuel Abatneh <91486438+samiabat@users.noreply.github.com>
2022-08-04 15:49:11 +0300

    Samuel.blog post (#284)
    * main branch
    
    * create folder to my project
    
    * add crud
    
    * fix post issue
    
    * finish create operation
    
    * add post
    
    * update completed
    
    * Update SamuelBlogPost.vue
    
    string literal rather than concat
    
    * Update SamuelAddBlog.vue
    
    remove unnecessary code.
    
    * Update SamuelAddBlog.vue
    
    change by object and remove unnecessary code
    
    * Update SamuelBlogPost.vue
    
    * Update SamuelBlogPost.vue
    
    * Update samuel.js
    
    * restore
    
    * fix issue
    
    * remove unnessasry code
    
    * add changes

---
+++ starter-project-web-vue2/pages/samuel/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-main class="white">
+      <BlogPost />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import BlogPost from '../../components/samuel/SamuelBlogPost.vue'
+export default {
+  name: 'IndexPage',
+  components: { BlogPost },
+}
+</script>
+<style>
+.padd {
+  padding-top: 5rem;
+}
+</style>
