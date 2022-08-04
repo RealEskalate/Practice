2794f0b6006ee21bd822055ba30122ce10224475
keiredin <se.keiredin.aman@gmail.com>
2022-08-04 21:45:54 +0300

    [web] add functionality to create a new blog

--- starter-project-web-vue2/components/keiredin/KeiredinNewBlog.vue
+++ starter-project-web-vue2/components/keiredin/KeiredinNewBlog.vue
@@ -1,35 +1,78 @@
 <template>
-  <v-dialog max-width="900px">
+<div>
+  <v-dialog  v-if="loggedIn" max-width="900px">
     <template #activator="{ on, attrs }">
       <v-btn color="primary" dark v-bind="attrs" v-on="on">
         <v-icon left> mdi-plus </v-icon>
         Add New Blog</v-btn
       >
     </template>
-    <v-card>
+    <v-card v-if="dialogVisible">
       <v-card-title>
         <h2>Add a New Blog</h2>
       </v-card-title>
       <v-card-text>
         <v-form class="px-3">
-          <v-text-field label="Title" />
-          <v-text-field label="Description" />
-          <v-textarea label="Content" />
+          <v-text-field v-model="title" label="Title" />
+          <v-text-field v-model="description" label="Description" />
+          <v-textarea v-model="content" label="Content" />
           <v-spacer></v-spacer>
-          <v-btn class="success mx-0 mt-3">Add Blog</v-btn>
+          <v-btn class="success mx-0 mt-3" @click="onSubmit">Add Blog</v-btn>
         </v-form>
       </v-card-text>
     </v-card>
   </v-dialog>
+  <v-btn
+    v-else
+    color="red"
+    dark
+    @click="gotoLogin()">
+    <v-icon left> mdi-plus </v-icon>
+    Add New Blog</v-btn
+  >
+</div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
 export default {
   name: 'AddBlog',
-  middleware({ store, redirect }) {
-    if (!store.state.auth.loggedIn) {
-      return redirect('/abraham/login')
+  data () {
+    return {
+      dialogVisible: false,
+      title: '',
+      description: '',
+      content: '',
+      newBlog: {}
+
     }
+  },
+
+  computed: { ...mapGetters('keiredin', ['loggedIn'] ) },
+  created() {
+    this.checkIfLoggedIn(this.$auth.loggedIn)
+  },
+  methods: {
+    ...mapActions('keiredin', ['checkIfLoggedIn', 'addBlog']),
+    gotoLogin (redirect) {
+      this.$router.push('/abraham/login')
+    },
+    onSubmit (e) {
+      e.preventDefault()
+      this.newBlog = {
+        title: this.title,
+        description: this.description,
+        content: this.content
+      }
+      this.addBlog(this.newBlog)
+      this.title =  ''
+      this.description = ''
+      this.content =  ''
+      this.dialogVisible = false
+
+
+    }
+
   },
 }
 </script>
--- starter-project-web-vue2/store/keiredin.js
+++ starter-project-web-vue2/store/keiredin.js
@@ -1,33 +1,47 @@
-import axios from 'axios'
-
 export const state = {
   blogs: [],
-  blog: {}
+  blog: {},
+  loggedIn: false
 }
 
 export const getters = {
   blogs: state => state.blogs,
-  blog: state => state.blog
+  blog: state => state.blog,
+  loggedIn: state => state.loggedIn
 }
 
 export const actions = {
+  checkIfLoggedIn ({commit}, auth) {
+    commit('setLoggedIn', auth)
+
+  },
   async fetchBlogs({ commit }) {
-    const response = await axios.get(
-      'https://blog-app-backend.onrender.com/api/articles/all'
+    const response = await this.$axios.get(
+      'articles/all'
     )
     commit('setBlogs', response.data)
   },
 
   async fetchBlog({ commit }, id) {
-    const response = await axios.get(
-      `https://blog-app-backend.onrender.com/api/articles/${id}`
+    const response = await this.$axios.get(
+      `articles/${id}`
     )
     commit('setBlog', response.data)
+  },
+
+  async addBlog({commit}, newBlog){
+    // const k = this.$auth.$storage.getLocalStorage('access_token')
+    // console.log(k)
+    const response = await this.$axios.post('articles', newBlog)
+    commit('setNewBlog', response.data)
+
   }
 }
 
 
 export const mutations = {
   setBlogs: (state, blogs) => (state.blogs = blogs),
-  setBlog: (state, blog) => (state.blog = blog)
+  setBlog: (state, blog) => (state.blog = blog),
+  setLoggedIn: (state, loggedIn) => (state.loggedIn = loggedIn),
+  setNewBlog: (state, newBlog) => (state.blogs.unshift(newBlog))
 }
