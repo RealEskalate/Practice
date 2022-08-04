2eb5f2b52576fe2d7c6b4ba352df7236cecf4876
AmsaleG <amsale@kemercode.com>
2022-08-04 09:52:58 +0300

    detail blog done

--- starter-project-web-vue2/components/amsale/AmsaleBlogList.vue
+++ starter-project-web-vue2/components/amsale/AmsaleBlogList.vue
@@ -1,52 +1,92 @@
 <template>
   <div>
     <AmsaleAddBlogVue />
-    <v-card class="mx-auto elevation-20 mt-5">
+    <div v-for="blog in blogs" :key="blog.id">
+       <v-card class="mx-auto elevation-20 mt-5">
       <v-row justify="space-between">
         <v-col cols="8">
           <v-card-title>
             <div>
-              <div class="text-h5">Halycon Days</div>
-              <p class="text-sm">
-                Ellie Goulding Halycon Days he People write alot about movies
-                even more than People write alot about movies even more than
-              </p>
-
-              <div>(2013)</div>
+              <div class="text-h5">{{blog.title}}</div>
+             
+               
             </div>
           </v-card-title>
+              <v-card-text>
+                {{blog.description}}
+                 <div class="">{{blog.updatedAt.slice(0,10)}}</div>
+              </v-card-text>
+
+             
         </v-col>
-        <v-img
-          class="ma-1"
-          contain
-          height="150px"
-          width="150px"
-          src="http://res.cloudinary.com/hakimhub/image/upload/v1654086909/starter_project/pcen6idszptplrwluiym.png"
-          style="flex-basis: 125px"
-        ></v-img>
+          <v-spacer></v-spacer>
+        <v-col >
+          
+          <v-btn
+            class="ma-2"
+            outlined
+            small
+            fab
+            color="green"
+          >
+            <v-icon>mdi-pencil</v-icon>
+          </v-btn>
+            <v-btn
+            class="ma-2"
+            outlined
+            small
+            fab
+            color="red"
+          >
+            <v-icon>mdi-delete</v-icon>
+          </v-btn>
+          
+        </v-col>
+      
       </v-row>
       <v-divider dark></v-divider>
       <v-card-actions class="pa-4">
-        <span class="grey--text text--lighten-1 text-caption mr-2">
-          Author Author name
+        <span class="grey--text text--lighten-1 text-caption mr-1">
+          <v-avatar
+            color="grey"
+            size="36"
+          >
+              <v-img
+              
+              contain
+              src="http://res.cloudinary.com/hakimhub/image/upload/v1654086909/starter_project/pcen6idszptplrwluiym.png"
+              style="flex-basis: 125px"
+            ></v-img>
+          </v-avatar> {{blog.authorUserId.fullName}}
         </span>
 
         <v-spacer></v-spacer>
         <nuxt-link
           style="text-decoration: none; color: inherit"
-          :to="'/amsale/id'"
+          :to="'/amsale/' + blog._id "
         >
           <v-btn text> Show more </v-btn>
         </nuxt-link>
       </v-card-actions>
+
     </v-card>
+    </div>
+   
   </div>
 </template>
 
 <script>
+import { mapState ,mapActions } from 'vuex';
 import AmsaleAddBlogVue from './AmsaleAddBlog.vue'
 export default {
   name: 'BlogList',
+  methods:{
+    ...mapActions('amsale',['fetchBlogs'])
+  },
+  computed:{...mapState('amsale',['blogs'])},
   components: { AmsaleAddBlogVue },
+  created(){
+    this.fetchBlogs();
+  }
 }
 </script>
--- starter-project-web-vue2/pages/amsale/_id.vue
+++ starter-project-web-vue2/pages/amsale/_id.vue
@@ -1,18 +1,79 @@
 <template>
   <v-card class="m-5">
-    <v-card-title>Title</v-card-title>
+    <v-card-title>{{title}}</v-card-title>
     <v-row>
+     
       <v-col>
+        <v-card-text>{{content}}</v-card-text>
+      </v-col>
+       <v-col>
         <v-img
           lazy-src="https://picsum.photos/id/11/10/6"
-          max-height="150"
-          max-width="250"
-          src="https://picsum.photos/id/11/500/300"
+          max-height="250"
+          max-width="550"
+          v-bind:src="img"
         ></v-img>
       </v-col>
-      <v-col>
-        <v-text></v-text>
-      </v-col>
     </v-row>
+     <v-divider dark></v-divider>
+     <v-card-actions>
+    
+        <v-list-item two-line>
+          <v-list-item-avatar>
+                <img src="https://picsum.photos/id/11/10/6">
+              </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>By : {{fullname}}</v-list-item-title>
+            <v-list-item-subtitle>{{email}}</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+         <v-spacer></v-spacer>
+        <v-rating
+            v-model="rating"
+            background-color="purple lighten-3"
+            color="purple"
+            small
+          ></v-rating>
+     </v-card-actions>
+   
   </v-card>
 </template>
+
+<script>
+  import axios from 'axios'
+  export default{
+    data(){
+      return {
+        content:'',
+        title:'',
+        rating:4,
+        fullname:"",
+        email:"",
+        img:""
+       
+      }
+    },
+    async created(){
+      const config = {
+        headers:{
+          Accept : 'application/json',
+        }
+      }
+      try{
+        const response = await axios.get( `https://blog-app-backend.onrender.com/api/articles/${this.$route.params.id}`,
+        config)
+        console.log(response)
+
+        this.content = response.data.content
+        this.title = response.data.title
+        this.fullname = response.data.authorUserId.fullName
+        this.email = response.data.authorUserId.email
+        this.img = response.data.imageUrls[0]
+
+      }
+      catch(err){
+        console.log(err)
+      }
+    }
+  }
+</script>
