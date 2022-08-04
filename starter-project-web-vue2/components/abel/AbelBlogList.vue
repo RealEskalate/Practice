d8b5cb33bbe860a756600579ba69e01012d0d9da
AtoBrightSide <meseleabel99@gmail.com>
2022-08-04 12:06:40 +0300

    [web] Incorporate Vuex to UI

---
+++ starter-project-web-vue2/components/abel/AbelBlogList.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-row>
+    <v-col v-for="blog in allBlogs" :key="blog.title" cols="12" sm="4">
+      <SingleBlog
+        :title="blog.title"
+        :fullName="blog.authorUserId.fullName"
+        :tag="blog.tag"
+        :content="blog.content"
+      />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import SingleBlog from '../../pages/abel/_id.vue'
+export default {
+  components: { SingleBlog },
+  computed: mapGetters('abel', ['allBlogs']),
+  created() {
+    this.fetchBlogs()
+  },
+  methods: {
+    ...mapActions('abel', ['fetchBlogs']),
+  },
+}
+</script>
--- starter-project-web-vue2/pages/index.vue
+++ starter-project-web-vue2/pages/index.vue
@@ -74,6 +74,11 @@ export default {
           description: 'Group 32 Student',
           link: '/yared',
         },
+        {
+          name: 'Abel Mesele',
+          description: 'Group 32 Student',
+          link: '/abel',
+        },
       ],
     }
   },
