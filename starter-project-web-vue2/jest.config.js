4ac91284f51ed44f4b5ee442ce03ea2e950e80af
danielgitk <danielk1781@gmail.com>
2022-08-04 11:37:46 +0300

    [web] add store files to coverage

--- starter-project-web-vue2/jest.config.js
+++ starter-project-web-vue2/jest.config.js
@@ -16,7 +16,9 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue'
+    '<rootDir>/pages/**/*.vue',
+    '<rootDir>/store/**/*.js',
+
   ],
   testEnvironment: 'jsdom'
 }
