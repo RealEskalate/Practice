0139a4b073079644f1edd0e0c7a4f06154808242
AtoBrightSide <meseleabel99@gmail.com>
2022-08-05 10:37:57 +0300

    [web]:Adds new features

--- starter-project-web-vue2/store/abel.js
+++ starter-project-web-vue2/store/abel.js
@@ -1,4 +1,3 @@
-
 const state = {
   blogs: [],
 }
@@ -9,20 +8,34 @@ const getters = {
 
 const actions = {
   async fetchBlogs({ commit }) {
-    const req = await this.$axios.get(
-      'articles/all'
-    )
+    const req = await this.$axios.get('articles/all')
     commit('setBlogs', req.data)
   },
   async addBlog({ commit }, blog) {
     const res = await this.$axios.post('articles/', blog)
     commit('addBlog', res.data)
   },
+  async deleteBlog({ commit }, id) {
+    await this.$axios.delete(`articles/${id}`)
+    commit('removeBlog', id)
+  },
+  async updateBlog({ commit }, updatedBlog) {
+    const req = await this.$axios.put(`articles/${updatedBlog.id}`, updatedBlog)
+    commit('updateBlog', req.data)
+  },
 }
 
 const mutations = {
   setBlogs: (state, blogs) => (state.blogs = blogs),
   addBlog: (state, blog) => state.blogs.unshift(blog),
+  removeBlog: (state, id) =>
+    (state.blogs = state.blogs.filter((blog) => blog.id !== id)),
+  updateBlog: (state, uptBlog) => {
+    const index = state.blogs.findIndex((blog) => blog.id === uptBlog.id)
+    if (index !== -1) {
+      state.blogs.splice(index, 1, uptBlog)
+    }
+  },
 }
 
 export default {
