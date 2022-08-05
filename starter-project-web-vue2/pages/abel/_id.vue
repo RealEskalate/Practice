86ebeb993dfe3a4cca485841a2a2932108629fd7
AtoBrightSide <meseleabel99@gmail.com>
2022-08-05 10:36:35 +0300

    [web]:address comment

--- starter-project-web-vue2/pages/abel/_id.vue
+++ starter-project-web-vue2/pages/abel/_id.vue
@@ -1,26 +1,73 @@
 <template>
-  <v-card class="mx-auto deep-purple" min-height="250" max-width="344">
-    <v-card-text>
-      <div>{{ fullName }}</div>
-      <p class="text-h5 text--primary">{{ title }}</p>
-      <div class="text--secondary">{{content}}</div>
-    </v-card-text>
-    <v-card-actions >
-      <v-btn text color="text--white"> Read blog </v-btn>
-      <v-btn text color="text--white"> Delete blog </v-btn>
-      <v-btn text color="text--white"> Update blog </v-btn>
-    </v-card-actions>
-  </v-card>
+  <v-container>
+    <v-form v-show="edit">
+      <v-text-field
+        v-model="blog.title"
+        type="text"
+        name="title"
+        label="Title"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="blog.content"
+        type="text"
+        name="content"
+        label="Content"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="blog.description"
+        type="text"
+        name="description"
+        label="Description"
+        required
+      ></v-text-field>
+      <v-btn @click="onSubmit" value="Submit" class="mb-3">Update Blog</v-btn>
+    </v-form>
+    <v-card elevation="10" class="pa-3">
+      <nuxt-link to="/abel/">
+        <v-btn>Go Back</v-btn>
+      </nuxt-link>
+      <v-card-title>
+        {{ blog.title }}
+      </v-card-title>
+      <v-card-text>
+        {{ blog.description }}
+      </v-card-text>
+      <v-card-text>{{ blog.content }}</v-card-text>
+      <v-card-actions>
+        <v-btn @click="toggleEdit">Edit blog</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'SingleBlog',
-  props: {
-    title: String,
-    tag: String,
-    content: String,
-    fullName: String
-  }
+  data() {
+    return {
+      blog: {},
+      edit: false,
+    }
+  },
+  async created() {
+    const res = await axios.get(
+      `https://blog-app-backend.onrender.com/api/articles/${this.$route.params.id}`
+    )
+    this.blog = res.data
+  },
+  methods: {
+    toggleEdit() {
+      this.edit = !this.edit
+    },
+  },
 }
 </script>
+
+<style>
+a {
+  text-decoration: none;
+}
+</style>
