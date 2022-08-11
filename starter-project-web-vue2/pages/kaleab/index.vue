49290c435c71246bc504b3bc60fdbf40371f2319
Kaleab Kindu <kkmichaelstarkk@gmail.com>
2022-08-12 01:43:46 +0300

    create landing page and a page for a single blog

---
+++ starter-project-web-vue2/pages/kaleab/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <Articles />
+  </v-app>
+</template>
+
+<script>
+import Articles from '../../components/kaleab/KaleabArticles.vue'
+
+
+export default {
+  name: 'MyPage',
+  components: {
+    Articles,
+},
+}
+</script>
